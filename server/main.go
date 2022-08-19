e111dee2ef2d9e75e184bd87dbb04eb7d575f26d
Gianluca Mondini <mondini@open2b.com>
2022-08-19 16:06:35 +0200

    Add endpoint 'run-query'

--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -74,6 +75,7 @@ func (server *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	rpath := r.URL.Path
 	rpath = strings.TrimSuffix(rpath, "/")
 	switch {
+
 	case strings.HasSuffix(rpath, "/log-event"):
 		var event *Event
 		err := json.NewDecoder(r.Body).Decode(&event)
@@ -87,9 +89,26 @@ func (server *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			log.Printf("[error] cannot log event: %s", err)
 			return
 		}
+
+	case strings.HasSuffix(rpath, "/run-query"):
+		var query string
+		err := json.NewDecoder(r.Body).Decode(&query)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		result, err := server.runQuery(query)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			log.Printf("[error] cannot log event: %s", err)
+			return
+		}
+		_ = json.NewEncoder(w).Encode(result)
+
 	default:
 		http.NotFound(w, r)
 		return
+
 	}
 }
 
@@ -101,6 +120,72 @@ func (server *Server) logEvent(e *Event) error {
 		"(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 	_, err := server.db.Exec(query, e.Timestamp, e.Language, e.Browser, e.URL, e.Referrer, e.Target, e.Event, e.Text, e.Title, e.Session)
 	return err
+}
+
+// runQuery runs the given query and returns its results as a [][]any.
+func (server *Server) runQuery(query string) ([][]any, error) {
+	rows, err := server.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	columnTypes, err := rows.ColumnTypes()
+	if err != nil {
+		return nil, err
+	}
+	columnsLen := len(columnTypes)
+	result := [][]any{}
+	for rows.Next() {
+		sqlRow := make([]any, columnsLen)
+		for j, column := range columnTypes {
+			switch column.DatabaseTypeName() {
+			case "TINYINT", "INT":
+				if _, nullable := column.Nullable(); nullable {
+					var value sql.NullInt64
+					sqlRow[j] = &value
+				} else {
+					var value int
+					sqlRow[j] = &value
+				}
+			case "BIGINT":
+				if _, nullable := column.Nullable(); nullable {
+					var value sql.NullInt64
+					sqlRow[j] = &value
+				} else {
+					var value int64
+					sqlRow[j] = &value
+				}
+			case "DATE":
+				var value time.Time
+				sqlRow[j] = &value
+			case "DATETIME":
+				var value time.Time
+				sqlRow[j] = &value
+			default:
+				if _, nullable := column.Nullable(); nullable {
+					var value sql.NullString
+					sqlRow[j] = &value
+				} else {
+					var value string
+					sqlRow[j] = &value
+				}
+			}
+		}
+		err := rows.Scan(sqlRow...)
+		if err != nil {
+			return nil, err
+		}
+		row := make([]any, len(sqlRow))
+		for i, pr := range sqlRow {
+			value, err := pr.(interface{ Value() (driver.Value, error) }).Value()
+			if err != nil {
+				panic(err)
+			}
+			row[i] = value
+		}
+		result = append(result, row)
+	}
+	return result, nil
 }
 
 type Event struct {
