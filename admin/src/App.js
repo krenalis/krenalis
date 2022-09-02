ce18bb45538766e50809ed7e70fc9972e7ef0246
Andrea Retini <retini@open2b.com>
2022-09-02 09:14:38 +0200

    admin: implement admin routing in React
    this commit also removes old css and images

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,81 +1,15 @@
-import './components/table';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar'
 import './App.css';
 
-let columns = [
-	{
-		code: 'id',
-		title: 'ID',
-		width: 100,
-	},
-	{
-		code: 'browser',
-		title: 'Browser',
-		width: 400,
-	},
-	{
-		code: 'eventType',
-		title: 'Event type',
-		width: 250,
-	},
-	{
-		code: 'language',
-		title: 'Language',
-		width: 200,
-	},
-	{
-		code: 'referrer',
-		title: 'Referrer',
-		width: 200,
-	},
-	{
-		code: 'session',
-		title: 'Session',
-		width: 250,
-	},
-	{
-		code: 'target',
-		title: 'Target',
-		width: 400,
-	},
-	{
-		code: 'text',
-		title: 'Text',
-		width: 300,
-	},
-	{
-		code: 'timestamp',
-		title: 'Timestamp',
-		width: 500,
-	},
-	{
-		code: 'title',
-		title: 'Title',
-		width: 300,
-	},
-	{
-		code: 'url',
-		title: 'URL',
-		width: 400,
-	},
-];
-
-function App() {
-	return (
-		<div className="App">
-			<h2>Altre pagine</h2>
-			<ul>
-				<li><a href="visualization">Query editor</a></li>
-			</ul>
-			<div className="page">
-				<o2b-table
-					title="Ultimi Eventi"
-					description="Gli ultimi eventi registrati sulla tua web app"
-					query="SELECT * FROM `events` ORDER BY `timestamp` DESC LIMIT 5;"
-					columns={JSON.stringify(columns)}
-				></o2b-table>
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<Sidebar />
+				<Outlet />
 			</div>
-		</div>
-	);
+		);
+	}
 }
-
-export default App;
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './routes/Home/Home'
+import Dashboard from './routes/Dashboard/Dashboard'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/admin/public/' element={<App />} >
+        <Route path='' element={<Home />} />
+        <Route path='dashboard' element={<Dashboard />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
