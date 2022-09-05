f7410c72b83a8b9c0faf323be40ada665d10bd75
Marco Gazerro <gazerro@open2b.com>
2022-09-05 16:12:26 +0200

    database: set the 'customers.password' column as ASCII

--- database/MySQL-updates.sql
+++ database/MySQL-updates.sql
@@ -4,3 +4,4 @@ ALTER TABLE `properties` CHANGE COLUMN `code` `id` CHAR(10) NOT NULL;
 RENAME TABLE `properties_domains` TO `domains`;
 ALTER TABLE `domains` CHANGE COLUMN `domain` `name` VARCHAR(255) NOT NULL;
 UPDATE `customers` SET `password` = '$2a$10$rlHZ0RUyMMeMQxDlAK6S2.sL7Y8Z8IafRsagpdYyadZrKpLJWH94K' WHERE `password` = 'foopass';
+ALTER TABLE `customers` CHANGE COLUMN `password` `password` CHAR(60) CHARACTER SET ascii NOT NULL DEFAULT '';
