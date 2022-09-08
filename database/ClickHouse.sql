cf413c8184fc4e3b7d837afa5aa5db9f30ea4875
Gianluca Mondini <mondini@open2b.com>
2022-09-08 10:30:41 +0200

    database: change the style of the 'CREATE TABLE events' query
    See the comment added to the file for more details.

--- database/ClickHouse.sql
+++ database/ClickHouse.sql
@@ -1,12 +1,16 @@
-CREATE TABLE `events`
+-- The "CREATE TABLE" statements in this file must have the same style of the
+-- queries obtained by running "SHOW CREATE TABLE" on ClickHouse (except for the
+-- database name on the query, which should be omitted in this file).
+
+CREATE TABLE events
 (
     `property` String,
     `timestamp` DateTime,
-    `osName` Enum('Android', 'Windows', 'iOS', 'MacOS', 'Linux', 'ChromeOS', 'Other'),
+    `osName` Enum8('Android' = 1, 'Windows' = 2, 'iOS' = 3, 'MacOS' = 4, 'Linux' = 5, 'ChromeOS' = 6, 'Other' = 7),
     `osVersion` String,
     `browserName` String,
     `browserVersion` String,
-    `deviceType` Enum('desktop', 'tablet', 'mobile'),
+    `deviceType` Enum8('desktop' = 1, 'tablet' = 2, 'mobile' = 3),
     `event` String,
     `language` String,
     `referrer` String,
