73617933f5e9fc971ac8af611af911ff7ead4559
Andrea Retini <retini@open2b.com>
2022-09-09 17:24:00 +0200

    trace-events-script: rewrite the tracking script
    This commit also extends the website for testing the tracking script

--- trace-events-script/chichi.js
+++ trace-events-script/chichi.js
@@ -1,82 +1,161 @@
-let endpoint = 'https://localhost:9090/log-event';
+(() => {
 
-let property = '1234567890'
-
-function sendBeacon(data) {
-	let d = data == null ? {} : data;
-
-	// populate the data object with relevant infos.
-	let device = localStorage.getItem('chichi-device');
-	if (device == null) {
-		device = makeid();
-		localStorage.setItem('chichi-device', device);
+	if (navigator == null) {
+		console.error(`[ChichiError] cannot run Chichi: browser doesn't support the Navigator interface`);
+		return;
 	}
-	d.device = device;
-	d.property = property;
-	navigator.geolocation.getCurrentPosition(
-		(position) => {
-			d.geolocation = {
-				latitude: position.coords.latitude,
-				longitude: position.coords.longitude,
-			};
-		},
-		(err) => {
-			console.log(`cannot get the geolocation: ${err.message}`);
+
+	let script = document.querySelector('script[data-isChichi]');
+	let endpoint = script.src;
+	let property = script.dataset.property;
+
+	// immediately send the pageView event.
+	sendEvent({ event: 'pageview' });
+
+	// when the DOM is fully parsed, attach the event listeners to all the
+	// clickable elements.
+	addEventListener('DOMContentLoaded', () => {
+		let clickableElements = document.querySelectorAll('a, button, input[type="submit"]');
+		for (let el of clickableElements) { el.addEventListener('click', sendClickEvent, false) }
+	});
+
+	function sendEvent(data) {
+		let d = data == null ? {} : data;
+
+		d.property = property;
+
+		let device;
+		if (isStorageAvailable('localStorage')) {
+			device = localStorage.getItem('chichi-device');
+			if (device == null) {
+				device = makeid();
+				localStorage.setItem('chichi-device', device);
+			}
 		}
-	);
-	d.referrer = document.referrer;
-	if (navigator.connection) {
-		d.connection = navigator.connection.type == null ? '' : navigator.connection.type;
+
+		d.referrer = document.referrer;
+
+		let n = navigator;
+		if (n.connection) d.connection = n.connection.type == null ? '' : n.connection.type;
+		if (n.language) d.language = n.language;
+		if (n.userAgentData) d.isMobile = n.userAgentData.mobile;
+
+		d.title = document.title;
+
+		d.url = window.location.href;
+
+		console.log(d);
+
+		// serialize the collected data in JSON.
+		let json;
+		try {
+			json = JSON.stringify(d);
+		} catch (err) {
+			console.error(`[ChichiError] cannot parse the collected data: ${err.message}`)
+		}
+
+		// send the JSON.
+		try {
+			n.sendBeacon(endpoint, json);
+		} catch (err) {
+			console.error(`[ChichiError] cannot send '${d.event}' event: ${err.message}`)
+		}
 	}
-	d.language = navigator.language;
-	if (navigator.userAgentData) {
-		d.os = navigator.userAgentData.platform;
-		d.isMobile = navigator.userAgentData.mobile;
+
+	function sendClickEvent(e) {
+		let text, target, event, initiator;
+		switch (e.currentTarget.tagName) {
+			case "A":
+				[text, target, event] = getAnchorData(e.currentTarget);
+				initiator = 'anchor';
+				break;
+			case "INPUT":
+				[text, target, event] = GetInputSubmitData(e.currentTarget);
+				initiator = 'input of type submit';
+				break;
+			case "BUTTON":
+				[text, target, event] = getButtonData(e.currentTarget);
+				initiator = 'button';
+				break;
+			default:
+				console.error(`[ChichiError] unexpected clickable element with tag '${e.currentTarget.tagName}'`);
+				return;
+		}
+		sendEvent({ event: event, target: target, text: text, initiator: initiator });
 	}
-	d.title = document.title;
-	d.url = window.location.href;
 
-	console.log(d);
-
-	// serialize the data.
-	let json;
-	try {
-		json = JSON.stringify(d);
-	} catch (err) {
-		console.error(`cannot parse Chichi's data`);
+	function getAnchorData(a) {
+		let text = a.textContent;
+		let target = a.href;
+		let event = 'click';
+		return [text, target, event];
 	}
 
-	// send the JSON.
-	navigator.sendBeacon(endpoint, json);
-}
+	function GetInputSubmitData(i) {
+		let text = i.value;
+		let target, event;
+		let form, formID;
+		if ((formID = i.getAttribute('form')) != null) {
+			form = document.querySelector(`form#${formID}`);
+		} else {
+			form = i.closest('form');
+		}
+		if (form == null) return [text, '', 'click'];
+		target = form.action;
+		formaction = i.getAttribute('formaction');
+		if (formaction != null && formaction != target) target = formaction;
+		event = 'form submission';
+		return [text, target, event];
+	}
 
-window.addEventListener(
-	'load',
-	(e) => {
-		sendBeacon({
-			event: 'pageview',
-		});
-	},
-	false
-);
+	function getButtonData(b) {
+		let text = b.textContent;
+		let target, event;
+		let isSubmit = b.type == 'submit';
+		if (!isSubmit) return [text, '', 'click'];
+		let form, formID;
+		if ((formID = b.getAttribute('form')) != null) {
+			form = document.querySelector(`form#${formID}`);
+		} else {
+			form = b.closest('form');
+		}
+		if (form == null) return [text, '', 'click'];
+		target = form.action;
+		formaction = b.getAttribute('formaction');
+		if (formaction != null && formaction != target) target = formaction;
+		event = 'form submission';
+		return [text, target, event];
+	}
 
-let anchors = document.querySelectorAll('a');
-for (let a of anchors) {
-	a.addEventListener(
-		'click',
-		(e) => {
-			sendBeacon({
-				target: e.currentTarget.href,
-				text: e.currentTarget.textContent,
-				event: 'click',
-			});
-		},
-		false
-	);
-}
+	function isStorageAvailable(type) {
+		let storage;
+		try {
+			storage = window[type];
+			const x = '__storage_test__';
+			storage.setItem(x, x);
+			storage.removeItem(x);
+			return true;
+		}
+		catch (e) {
+			return e instanceof DOMException && (
+				// everything except Firefox
+				e.code === 22 ||
+				// Firefox
+				e.code === 1014 ||
+				// test name field too, because code might not be present
+				// everything except Firefox
+				e.name === 'QuotaExceededError' ||
+				// Firefox
+				e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+				// acknowledge QuotaExceededError only if there's something already stored
+				(storage && storage.length !== 0);
+		}
+	}
 
-function makeid() {
-	let array = new Uint8Array(20);
-	self.crypto.getRandomValues(array);
-	return btoa(String.fromCharCode.apply(null, array));
-}
+	function makeid() {
+		let array = new Uint8Array(20);
+		self.crypto.getRandomValues(array);
+		return btoa(String.fromCharCode.apply(null, array));
+	}
+
+})();
