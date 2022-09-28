3f4ca769f90b23adeafba53ac90628999fadff39
Marco Gazerro <gazerro@open2b.com>
2022-09-28 18:00:40 +0200

    connectors/hubspot: document the 'Connecter' interface and its methods

--- connectors/connectors.go
+++ connectors/connectors.go
@@ -13,11 +13,22 @@ import (
 	"reflect"
 )
 
+// Connecter is the interface implemented by the connectors.
 type Connecter interface {
+
+	// Groups returns the groups starting from the given cursor.
 	Groups(ctx context.Context, account, cursor string, properties []string) error
+
+	// Properties returns all contact and company properties.
 	Properties(ctx context.Context, account string) ([]Property, error)
+
+	// ServeWebhook serves a webhook request.
 	ServeWebhook(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+
+	// SetUsers sets the given users.
 	SetUsers(ctx context.Context, token string, users []User) error
+
+	// Users returns the users starting from the given cursor.
 	Users(ctx context.Context, account, cursor string, properties []string) error
 }
 
