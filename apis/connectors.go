1d1434fdf0b7fccb5390e8a107542a06f334571d
Marco Gazerro <gazerro@open2b.com>
2022-10-07 17:10:26 +0200

    all: add missing file headers

--- apis/connectors.go
+++ apis/connectors.go
@@ -1,3 +1,10 @@
+//
+// SPDX-License-Identifier: Elastic-2.0
+//
+//
+// Copyright (c) 2002-2022 Open2b
+//
+
 package apis
 
 import (
@@ -6,6 +13,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
 )
 
 type Connectors struct {
