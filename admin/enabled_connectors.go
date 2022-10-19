d7145f218fb2b40e7a39771d74dee7aaef25a0e1
Gianluca Mondini <mondini@open2b.com>
2022-10-19 14:28:24 +0200

    admin: remove an useless TODO

--- admin/enabled_connectors.go
+++ admin/enabled_connectors.go
@@ -5,9 +5,6 @@
 // Copyright (c) 2002-2022 Open2b
 //
 
-// TODO(Gianluca): this file is necessary for prototyping; investigate if it's
-// necessary to keep this or not.
-
 package admin
 
 import (
