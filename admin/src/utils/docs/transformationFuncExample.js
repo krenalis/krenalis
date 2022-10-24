f5c833ca7eb641da063d243a05bd620f3f685457
Gianluca Mondini <mondini@open2b.com>
2022-10-24 14:32:19 +0200

    admin: restore transformation function example
    The transformation function was changed by mistake by the commit
    614d9770e14a51004034009cd1ecc4db56bda1a3.

--- admin/src/utils/docs/transformationFuncExample.js
+++ admin/src/utils/docs/transformationFuncExample.js
@@ -1,13 +1,17 @@
-export let transformationFuncExample = `func(input map[string]any) (map[string]any, error) {
+export let transformationFuncExample = `func(input map[string]any, timestamps map[string]time.Time) (map[string]any, map[string]time.Time, error) {
     out := map[string]any{}
+    outTimestamps := map[string]time.Time{}
     if firstName, ok := input["firstname"]; ok {
         out["FirstName"] = firstName
+        outTimestamps["FirstName"] = timestamps["firstname"]
     }
     if lastName, ok := input["lastname"]; ok {
         out["LastName"] = lastName
+        outTimestamps["LastName"] = timestamps["lastname"]
     }
     if email, ok := input["email"]; ok {
         out["Email"] = email
+        outTimestamps["Email"] = timestamps["email"]
     }
-    return out, nil
+    return out, outTimestamps, nil
 }`
