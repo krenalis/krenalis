5891eef7df4276a53ac7e5ea943a7e2ce5eed1cc
Marco Gazerro <gazerro@open2b.com>
2022-11-08 10:48:21 +0100

    connectors/s3: fix region validation

--- connectors/s3/s3.go
+++ connectors/s3/s3.go
@@ -128,7 +128,7 @@ func (c *connection) ServeUI(event string, values []byte) (*ui.Form, error) {
 		const regions = "us-east-1 us-east-2 us-west-1 us-west-2 af-south-1 ap-east-1 ap-southeast-3 ap-south-1 " +
 			"ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 " +
 			"eu-west-1 eu-west-2 eu-west-3 eu-south-1 eu-north-1 me-south-1 me-central-1 sa-east-1"
-		if !strings.Contains(regions, s.Region+" ") || !strings.HasSuffix(regions, " "+s.Region) {
+		if !strings.Contains(regions, s.Region+" ") && !strings.HasSuffix(regions, " "+s.Region) {
 			return nil, ui.Errorf("region is not valid")
 		}
 		// Validate Bucket.
