ee96c423ba75902c04bc3e7317e696c25de5e1f7
Gianluca Mondini <mondini@open2b.com>
2022-12-09 12:22:36 +0100

    database: set a default value for the 'golden_record' column
    This prevents the following error when executing the queries in the
    'writeConnectionUsers' method:
    
    "null value in column "golden_record" of relation "connections_users"
    violates not-null constraint (SQLSTATE 23502)"

--- database/PostgreSQL.sql
+++ database/PostgreSQL.sql
@@ -113,7 +113,7 @@ CREATE TABLE connections_users (
     "user" varchar(45) NOT NULL DEFAULT '',
     data varchar(655359) NOT NULL,
     timestamps varchar(655359) NOT NULL DEFAULT '',
-    golden_record integer NOT NULL,
+    golden_record integer NOT NULL DEFAULT 0,
     PRIMARY KEY (connection, "user")
 );
 
