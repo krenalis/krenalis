c4fac1befe80193d5953203f9daf3173ea5d4f83
Andrea Retini <retini@open2b.com>
2023-01-16 16:57:01 +0100

    admin/src: rewrite `App` component using hooks

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,12 +6,12 @@ import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.j
 
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.85/dist/');
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<div className='App'>
-				<Outlet />
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className='App'>
+			<Outlet />
+		</div>
+	);
+};
+
+export default App;
