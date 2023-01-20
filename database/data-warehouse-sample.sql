be33d51698718a86b1084d9f9ae154008c858bb3
Gianluca Mondini <mondini@open2b.com>
2023-01-20 14:01:48 +0100

    database: add some columns to the 'users' table of the warehouse

--- database/data-warehouse-sample.sql
+++ database/data-warehouse-sample.sql
@@ -1,11 +1,16 @@
-
 CREATE TYPE gender AS ENUM ('male', 'female', 'other');
 
+CREATE TYPE drink AS ENUM ('water', 'wine', 'beer', 'tea');
+
+CREATE TYPE fruit AS ENUM ('apple', 'orange', 'mango', 'peach', 'lime');
+
 CREATE TABLE users (
-    id          SERIAL,
-    "FirstName" varchar(300),
-    "LastName"  varchar(300),
-    "Email"     varchar(300),
-    "Gender"    gender,
+    id                      SERIAL,
+    "FirstName"             varchar(300),
+    "LastName"              varchar(300),
+    "Email"                 varchar(300),
+    "Gender"                gender,
+    "FoodPreferences_Drink" drink,
+    "FoodPreferences_Fruit" fruit,
     PRIMARY KEY (id)
 );
