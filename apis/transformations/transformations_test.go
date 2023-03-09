b01fe6f7dcb35977ccc38e16d2e28dc5fe5f330e
Gianluca Mondini <mondini@open2b.com>
2023-03-09 15:30:25 +0100

    apis/transformations: add a benchmark on function `Run2`

--- apis/transformations/transformations_test.go
+++ apis/transformations/transformations_test.go
@@ -49,3 +49,21 @@ func TestRun(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkRun2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pool := transformations.NewPool()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		src := `
+def transform(user):
+	return user`
+		got, err := pool.Run2(ctx, src, map[string]any{"Name": "John", "LastName": "Lennon"})
+		if err != nil {
+			b.Fatal(err)
+		}
+		if !reflect.DeepEqual(got, map[string]any{"Name": "John", "LastName": "Lennon"}) {
+			b.Fatalf("bad")
+		}
+	}
+}
