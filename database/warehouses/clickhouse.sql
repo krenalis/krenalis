d1dff6777437e52763e92fe4051fa51bcefa7cf4
Gianluca Mondini <mondini@open2b.com>
2023-04-17 11:03:22 +0200

    database/warehouses/clickhouse: add missing comma to query

--- database/warehouses/clickhouse.sql
+++ database/warehouses/clickhouse.sql
@@ -14,7 +14,7 @@ CREATE TABLE users
     -- "FoodPreferences_Drink" and "FoodPreferences_Fruit" to avoid grouping
     -- because the method "Workspace.Users" does not handle nested properties
     -- correctly.
-    `PhoneNumbers` Array(String)
+    `PhoneNumbers` Array(String),
     `FoodPreferences_Fruit` Enum8('apple' = 1, 'orange' = 2, 'mango' = 3, 'peach' = 4, 'lime' = 5),
 )
 ENGINE = MergeTree
