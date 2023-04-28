5263a67b295d5522607adead3aa2d5076fc47d1c
Marco Gazerro <gazerro@open2b.com>
2023-04-28 16:45:53 +0200

    connectors/parquet: remove a TODO

--- connectors/parquet/parquet.go
+++ connectors/parquet/parquet.go
@@ -260,7 +260,7 @@ func propertyType(column string, elem *parquet.SchemaElement) (types.Type, error
 			return types.Date(""), nil // TODO(marco) set the layout
 		}
 		if lt.TIMESTAMP != nil {
-			return types.DateTime(""), nil // TODO(marco) set the layout
+			return types.DateTime(""), nil
 		}
 		if lt.TIME != nil {
 			return types.Time(), nil
