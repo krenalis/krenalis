krenalis/krenalis

commit 99e976a09dd290d85f6d8addb3b50f8f9035f146

apis: make state-changing methods return after the state is changed
Before this commit, an API method could change the state in the database
but would return before the state change was fully propagated. This
commit ensures that the state is propagated to the current node before
returning.

However, it does not guarantee that the change is propagated to the
other nodes before returning.

For #193

apis/apis.go
@@ -120,9 +120,6 @@ func New(ctx context.Context, conf *Config) (*APIs, error) {
 		return nil, err
 	}
 
-	// Keep the state updated.
-	apis.state.Keep()
-
 	return apis, nil
 }
 

apis/state/load.go
@@ -53,6 +53,9 @@ func Load(ctx context.Context, db *postgres.DB) (*State, error) {
 		resources:        map[int]*Resource{},
 	}
 
+	// Keep the state updated.
+	go state.keepState()
+
 	n := LoadStateNotification{ID: state.id}
 
 	err = state.db.Transaction(ctx, func(tx *postgres.Tx) error {

apis/postgres/notifications.go
@@ -13,17 +13,25 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
+	"math"
+	"math/rand"
 	"reflect"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
+
+const maxIDLen = len("9223372036854775807")
 
 type Notification struct {
 	PID     uint32
 	Name    string
 	Payload string
+	Ack     chan<- struct{}
 }
 
 // Notify sends a notification.
@@ -54,7 +62,7 @@ func notify(ctx context.Context, conn Connection, payload any) error {
 	s := b.String()
 	s = s[:len(s)-1] // remove new line added by Encode.
 	s = escape(s)
-	if len(s) > 8000-2 {
+	if len(s) > 8000-maxIDLen-2 {
 		if db, ok := conn.(*DB); ok {
 			// Send within a transaction.
 			return db.Transaction(ctx, func(tx *Tx) error {
@@ -77,14 +85,19 @@ func notify(ctx context.Context, conn Connection, payload any) error {
 			return err
 		}
 		s = z.String()
-		for len(s) > 8000-2 {
-			const k = 8000 - 3
+		for len(s) > 8000-maxIDLen-2 {
+			const k = 8000 - maxIDLen - 3
 			_, err = conn.Exec(ctx, "NOTIFY chichi, '+"+s[:k]+"'")
 			if err != nil {
 				return err
 			}
 			s = s[k:]
 		}
+	}
+	if tx, ok := conn.(*Tx); ok {
+		id, ack := tx.acks.create()
+		tx.ack = ack
+		s += strconv.Itoa(id)
 	}
 	_, err = conn.Exec(ctx, "NOTIFY chichi, '"+s+"'")
 	return err
@@ -151,11 +164,15 @@ func (db *DB) ListenToNotifications(ctx context.Context) <-chan Notification {
 						payload = s.String()
 						b.Reset()
 					}
-					i := strings.IndexByte(payload, '{')
-					if i == -1 {
-						i = len(payload)
+					id, name, payload, err := parsePayload(payload)
+					if err != nil {
+						return err
 					}
-					ch <- Notification{n.PID, payload[:i], payload[i:]}
+					var ack chan<- struct{}
+					if id > 0 {
+						ack = db.acks.pop(id)
+					}
+					ch <- Notification{n.PID, name, payload, ack}
 				}
 			}()
 			if err != nil {
@@ -166,4 +183,73 @@ func (db *DB) ListenToNotifications(ctx context.Context) <-chan Notification {
 		}
 	}()
 	return ch
+}
+
+// parsePayload parses a notification payload and returns the identifier, name,
+// and effective payload of the notification. If there is no identifier, it
+// returns 0 as identifier.
+func parsePayload(s string) (id int, name, payload string, err error) {
+	i := strings.IndexByte(s, '{')
+	if i == -1 {
+		return 0, "", "", errors.New("missing payload")
+	}
+	if i == 0 {
+		return 0, "", "", errors.New("missing name")
+	}
+	name, s = s[:i], s[i:]
+	i = strings.LastIndexByte(s, '}')
+	if i == -1 {
+		return 0, "", "", errors.New("invalid payload")
+	}
+	payload, s = s[:i+1], s[i+1:]
+	if s == "" {
+		return
+	}
+	id, _ = strconv.Atoi(s)
+	if id < 1 {
+		return 0, "", "", errors.New("invalid identifier")
+	}
+	return
+}
+
+// acks contains channels used by transactions for which a notification has
+// been sent. These channels are used to receive an acknowledgment when the
+// notification has been received and processed.
+type acks struct {
+	sync.Mutex
+	ids map[int]chan struct{}
+}
+
+// newAcks returns a new empty acks.
+func newAcks() *acks {
+	return &acks{ids: map[int]chan struct{}{}}
+}
+
+// create creates a new ack channel and returns it with its identifier.
+func (acks *acks) create() (int, <-chan struct{}) {
+	var id int
+	var ack chan struct{}
+	for ack == nil {
+		id = rand.Intn(math.MaxInt-1) + 1
+		acks.Lock()
+		_, ok := acks.ids[id]
+		if !ok {
+			ack = make(chan struct{}, 1)
+			acks.ids[id] = ack
+		}
+		acks.Unlock()
+	}
+	return id, ack
+}
+
+// pop removes the ack channel with identifier id and returns it. Returns nil
+// if the ack channel does not exist.
+func (acks *acks) pop(id int) chan<- struct{} {
+	acks.Lock()
+	ack, ok := acks.ids[id]
+	if ok {
+		delete(acks.ids, id)
+	}
+	acks.Unlock()
+	return ack
 }

apis/postgres/notifications_test.go
@@ -1,0 +1,57 @@
+//
+// SPDX-License-Identifier: Elastic-2.0
+//
+//
+// Copyright (c) 2023 Open2b
+//
+
+package postgres
+
+import (
+	"testing"
+)
+
+func TestParsePayload(t *testing.T) {
+
+	tests := []struct {
+		notification string
+		id           int
+		name         string
+		payload      string
+		err          bool
+	}{
+		{`foo{}`, 0, `foo`, `{}`, false},
+		{`foo{}5`, 5, `foo`, `{}`, false},
+		{`boo{"a":{"b":5}}6301`, 6301, `boo`, `{"a":{"b":5}}`, false},
+		{``, 0, ``, ``, true},
+		{`{}`, 0, ``, ``, true},
+		{`boo`, 0, ``, ``, true},
+		{`boo123`, 0, ``, ``, true},
+		{`boo{}0`, 0, ``, ``, true},
+		{`boo{}-1`, 0, ``, ``, true},
+		{`boo{} 5`, 0, ``, ``, true},
+	}
+
+	for _, test := range tests {
+		id, name, payload, err := parsePayload(test.notification)
+		if err != nil {
+			if !test.err {
+				t.Fatalf("%s: cannot parse notification: %s", test.notification, err)
+			}
+			continue
+		}
+		if test.err {
+			t.Fatalf("%s: expected error, got no errors", test.notification)
+		}
+		if id != test.id {
+			t.Fatalf("%s: expected identifier %d, got %d", test.notification, test.id, id)
+		}
+		if name != test.name {
+			t.Fatalf("%s: expected name %q, got %q", test.notification, test.name, name)
+		}
+		if payload != test.payload {
+			t.Fatalf("%s: expected payload %q, got %q", test.notification, test.payload, payload)
+		}
+	}
+
+}

apis/postgres/postgres.go
@@ -111,8 +111,9 @@ var (
 )
 
 type DB struct {
-	db  *pgxpool.Pool
-	log io.Writer
+	db   *pgxpool.Pool
+	acks *acks
+	log  io.Writer
 }
 
 type Options struct {
@@ -153,7 +154,7 @@ func Open(opts *Options) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &DB{conn, nil}, nil
+	return &DB{db: conn, acks: newAcks()}, nil
 }
 
 func (db *DB) Close() {
@@ -280,29 +281,33 @@ func (db *DB) Begin(ctx context.Context) (*Tx, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Tx{tx, db.log, false, false}, nil
+	return &Tx{tx, db.acks, db.log, false, false, nil}, nil
 }
 
 func (db *DB) Transaction(ctx context.Context, f func(tx *Tx) error) error {
-	var tx, err = db.db.Begin(ctx)
+	pqTx, err := db.db.Begin(ctx)
 	if err != nil {
 		return err
 	}
 	return func() error {
 		defer func() {
 			if err := recover(); err != nil {
-				_ = tx.Rollback(ctx)
+				_ = pqTx.Rollback(ctx)
 				panic(err)
 			}
 		}()
-		var err = f(&Tx{tx, db.log, true, false})
+		tx := &Tx{pqTx, db.acks, db.log, true, false, nil}
+		err := f(tx)
 		if err != nil {
-			_ = tx.Rollback(ctx)
+			_ = pqTx.Rollback(ctx)
 			return err
 		}
-		err = tx.Commit(ctx)
+		err = pqTx.Commit(ctx)
 		if err != nil && err != sql.ErrTxDone {
 			return err
+		}
+		if tx.ack != nil {
+			<-tx.ack
 		}
 		return nil
 	}()
@@ -518,9 +523,11 @@ func (c *Conn) Close(ctx context.Context) error {
 
 type Tx struct {
 	tx            pgx.Tx
+	acks          *acks
 	log           io.Writer
 	wrapped       bool
 	preparedQuery bool
+	ack           <-chan struct{}
 }
 
 func (tx *Tx) Exec(ctx context.Context, query string, args ...any) (*Result, error) {

apis/state/keep.go
@@ -64,11 +64,6 @@ func (state *State) AddListener(listener any) {
 	}
 }
 
-// Keep keeps the state updated.
-func (state *State) Keep() {
-	go state.keepState()
-}
-
 // keepState keeps the state in sync with the database. It is called in its own
 // goroutine.
 func (state *State) keepState() {
@@ -86,6 +81,9 @@ func (state *State) keepState() {
 				n.PID, n.Name, n.Payload)
 		}
 		if !state.syncing && n.Name != "LoadState" {
+			if n.Ack != nil {
+				n.Ack <- struct{}{}
+			}
 			continue
 		}
 		switch n.Name {
@@ -145,6 +143,9 @@ func (state *State) keepState() {
 			state.setWorkspacePrivacyRegion(n)
 		default:
 			log.Printf("[warning] unknown notification %q received from %d: %s", n.Name, n.PID, n.Payload)
+		}
+		if n.Ack != nil {
+			n.Ack <- struct{}{}
 		}
 	}
 

test/chichitester/call.go
@@ -16,7 +16,6 @@ import (
 	"net/http"
 	"runtime/debug"
 	"strings"
-	"time"
 )
 
 // StatusCodeError is an error returned by Call when the HTTP call returned a
@@ -85,11 +84,6 @@ func (c *Chichi) call(httpMethod, method string, body any) (any, error) {
 		}
 		return nil, &StatusCodeError{Code: resp.StatusCode, ResponseText: string(text)}
 	}
-
-	// TODO(Gianluca): add a brief timeout to mitigate the possibility that the
-	// state of Chichi has still not been updated after receiving this call. For
-	// more details see the issue https://github.com/open2b/chichi/issues/193.
-	time.Sleep(30 * time.Millisecond)
 
 	var out any
 	err = json.NewDecoder(resp.Body).Decode(&out)

commit 389139da054e3056634856acd0dd75e0a218287e

apis,apis/state: fix state load and listeners
This commit fixes a bug, introduced in commit 99e976a, where the
listeners on the state may not receive the first events received after
loading the state. This happens because, mistakenly, the listeners are
added after loading the state instead of before, as it should be.

To fix this, this commit separates the creation of the state, using the
new `state.New` function, from the loading of the state.

apis/apis.go
@@ -101,19 +101,25 @@ func New(ctx context.Context, conf *Config) (*APIs, error) {
 
 	apis := &APIs{db: db, redis: redisClient}
 
-	// Load the state.
-	apis.state, err = state.Load(ctx, db)
-	if err != nil {
-		return nil, err
-	}
-
 	// Set the HTTP client.
 	apis.http = httpclient.New(db, apis.state, http.DefaultTransport)
 	apis.http.SetTrace(os.Stdout)
 
+	// Instantiate the state.
+	apis.state, err = state.New(ctx, db)
+	if err != nil {
+		return nil, err
+	}
+
 	// Listen to state changes.
 	apis.state.AddListener(apis.onElectLeader)
 	apis.state.AddListener(apis.onExecuteAction)
+
+	// Load the state.
+	err = apis.state.Load()
+	if err != nil {
+		return nil, err
+	}
 
 	apis.events, err = events.New(ctx, db, redisClient, apis.state, apis.http)
 	if err != nil {

apis/state/load.go
@@ -8,7 +8,6 @@
 package state
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"reflect"
@@ -18,8 +17,6 @@ import (
 	"chichi/apis/postgres"
 	"chichi/connector"
 	"chichi/connector/types"
-
-	"github.com/google/uuid"
 )
 
 var (
@@ -30,35 +27,17 @@ var (
 	sheetsType              = reflect.TypeOf((*connector.Sheets)(nil)).Elem()
 )
 
-// Load loads the state and returns it.
-func Load(ctx context.Context, db *postgres.DB) (*State, error) {
-
-	id, err := uuid.NewUUID()
-	if err != nil {
-		return nil, err
-	}
-
-	state := &State{
-		id:               id,
-		db:               db,
-		mu:               new(sync.Mutex),
-		ctx:              ctx,
-		notifications:    db.ListenToNotifications(ctx),
-		accounts:         map[int]*Account{},
-		connectors:       map[int]*Connector{},
-		workspaces:       map[int]*Workspace{},
-		connections:      map[int]*Connection{},
-		connectionsByKey: map[string]*Connection{},
-		actions:          map[int]*Action{},
-		resources:        map[int]*Resource{},
-	}
+// Load loads the state.
+func (state *State) Load() error {
 
 	// Keep the state updated.
 	go state.keepState()
 
 	n := LoadStateNotification{ID: state.id}
 
-	err = state.db.Transaction(ctx, func(tx *postgres.Tx) error {
+	ctx := state.ctx
+
+	err := state.db.Transaction(ctx, func(tx *postgres.Tx) error {
 
 		// Read the latest election.
 		err := state.db.QueryRow(ctx, "SELECT number, leader FROM election LIMIT 1").
@@ -429,9 +408,6 @@ func Load(ctx context.Context, db *postgres.DB) (*State, error) {
 
 		return tx.Notify(ctx, n)
 	})
-	if err != nil {
-		return nil, err
-	}
 
-	return state, nil
+	return err
 }

apis/state/state.go
@@ -64,6 +64,32 @@ type State struct {
 	}
 }
 
+// New returns a *State instance.
+func New(ctx context.Context, db *postgres.DB) (*State, error) {
+
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	state := &State{
+		id:               id,
+		db:               db,
+		mu:               new(sync.Mutex),
+		ctx:              ctx,
+		notifications:    db.ListenToNotifications(ctx),
+		accounts:         map[int]*Account{},
+		connectors:       map[int]*Connector{},
+		workspaces:       map[int]*Workspace{},
+		connections:      map[int]*Connection{},
+		connectionsByKey: map[string]*Connection{},
+		actions:          map[int]*Action{},
+		resources:        map[int]*Resource{},
+	}
+
+	return state, nil
+}
+
 // Account returns the account with identifier id.
 // The boolean return value reports whether the account exists.
 func (state *State) Account(id int) (*Account, bool) {