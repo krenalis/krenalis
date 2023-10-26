e124754d39d6725ed02282506e4d4d044922b673
Gianluca Mondini <mondini@open2b.com>
2023-10-26 09:47:11 +0200

    apis/datastore/warehouses/postgresql: improve indentation in SQL file

--- apis/datastore/warehouses/postgresql/stored_procedure_queries.sql
+++ apis/datastore/warehouses/postgresql/stored_procedure_queries.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE FUNCTION matching_func(VARIADIC identifiers text[])
-    RETURNS boolean -- may return "null" to indicate "i don't know"
-    AS $$
-        DECLARE
-            i INT;
-        BEGIN
-            FOR i IN 1..array_length(identifiers, 1) BY 2 LOOP
-                IF identifiers[i] IS NOT NULL AND identifiers[i+1] IS NOT NULL THEN
-                    RETURN identifiers[i] = identifiers[i+1];
-                END IF;
-            END loop;
-            RETURN null;
-        END;
-    $$ LANGUAGE plpgsql;
+RETURNS boolean -- may return "null" to indicate "i don't know"
+AS $$
+    DECLARE
+        i INT;
+    BEGIN
+        FOR i IN 1..array_length(identifiers, 1) BY 2 LOOP
+            IF identifiers[i] IS NOT NULL AND identifiers[i+1] IS NOT NULL THEN
+                RETURN identifiers[i] = identifiers[i+1];
+            END IF;
+        END loop;
+        RETURN null;
+    END;
+$$ LANGUAGE plpgsql;
 
 DROP TABLE IF EXISTS "matchings";
 CREATE TABLE matchings (
@@ -24,83 +24,83 @@ DROP TABLE IF EXISTS clusters_to_merge;
 CREATE TABLE clusters_to_merge(c1 int, c2 int);
 
 CREATE OR REPLACE PROCEDURE resolve_sync_users()
-    LANGUAGE sql
-    AS $$
+LANGUAGE sql
+AS $$
 
-        -- Determine the matchings.
-        TRUNCATE matchings;
-        INSERT INTO
-            matchings(i1, i2, match)
-        SELECT
-            i1.__identity_id__,
-            i2.__identity_id__,
-            
-            -- This placeholder will be replaced by Chichi:
-            {{ matching_expr }} as match
-        FROM
-            users_identities i1
-                CROSS JOIN
-            users_identities i2
-        WHERE
-            i1.__identity_id__ < i2.__identity_id__;
+    -- Determine the matchings.
+    TRUNCATE matchings;
+    INSERT INTO
+        matchings(i1, i2, match)
+    SELECT
+        i1.__identity_id__,
+        i2.__identity_id__,
         
-        -- Do the clustering.
-        DO $clustering$
-            DECLARE
-                has_clusters_to_merge boolean;
-            BEGIN 
-        
-            LOOP
-            
-                -- Determine the clusters to merge.
-                TRUNCATE clusters_to_merge;
-                INSERT INTO
-                    clusters_to_merge(c1, c2)
-                SELECT
-                    i1.__cluster__ c1,
-                    i2.__cluster__ c2
-                FROM
-                    matchings m
-                    JOIN users_identities i1 ON m.i1 = i1.__identity_id__
-                    JOIN users_identities i2 ON m.i2 = i2.__identity_id__
-                WHERE
-                    m.match
-                    AND i1.__cluster__ <> i2.__cluster__;
-
-                -- Stop iterating when there are no more clusters to merge.
-                SELECT count(*) > 0 INTO has_clusters_to_merge FROM clusters_to_merge;
-                EXIT WHEN NOT has_clusters_to_merge;
-
-                -- Make the "clusters_to_merge" table simmetric.
-                -- TODO(Gianluca): is this necessary?
-                INSERT INTO clusters_to_merge
-                    SELECT c2, c1
-                    FROM clusters_to_merge;
-                
-                -- Update the clusters of the user identities.
-                UPDATE
-                    users_identities identities_a
-                SET
-                    __cluster__ = least(identities_a.__cluster__, target)
-                FROM
-                    users_identities identities_b
-                    JOIN (
-                        SELECT
-                            c1 source,
-                            min(c2) target
-                        FROM
-                            clusters_to_merge
-                        GROUP BY
-                            source
-                    ) new_clusters ON new_clusters.source = identities_b.__cluster__
-                WHERE
-                    identities_a.__identity_id__ = identities_b.__identity_id__;
-
-            END LOOP;
-
-        END $clustering$;
-
         -- This placeholder will be replaced by Chichi:
-        {{ users_sync_queries }}
+        {{ matching_expr }} as match
+    FROM
+        users_identities i1
+            CROSS JOIN
+        users_identities i2
+    WHERE
+        i1.__identity_id__ < i2.__identity_id__;
+    
+    -- Do the clustering.
+    DO $clustering$
+        DECLARE
+            has_clusters_to_merge boolean;
+        BEGIN 
+    
+        LOOP
+        
+            -- Determine the clusters to merge.
+            TRUNCATE clusters_to_merge;
+            INSERT INTO
+                clusters_to_merge(c1, c2)
+            SELECT
+                i1.__cluster__ c1,
+                i2.__cluster__ c2
+            FROM
+                matchings m
+                JOIN users_identities i1 ON m.i1 = i1.__identity_id__
+                JOIN users_identities i2 ON m.i2 = i2.__identity_id__
+            WHERE
+                m.match
+                AND i1.__cluster__ <> i2.__cluster__;
 
-    $$;
+            -- Stop iterating when there are no more clusters to merge.
+            SELECT count(*) > 0 INTO has_clusters_to_merge FROM clusters_to_merge;
+            EXIT WHEN NOT has_clusters_to_merge;
+
+            -- Make the "clusters_to_merge" table simmetric.
+            -- TODO(Gianluca): is this necessary?
+            INSERT INTO clusters_to_merge
+                SELECT c2, c1
+                FROM clusters_to_merge;
+            
+            -- Update the clusters of the user identities.
+            UPDATE
+                users_identities identities_a
+            SET
+                __cluster__ = least(identities_a.__cluster__, target)
+            FROM
+                users_identities identities_b
+                JOIN (
+                    SELECT
+                        c1 source,
+                        min(c2) target
+                    FROM
+                        clusters_to_merge
+                    GROUP BY
+                        source
+                ) new_clusters ON new_clusters.source = identities_b.__cluster__
+            WHERE
+                identities_a.__identity_id__ = identities_b.__identity_id__;
+
+        END LOOP;
+
+    END $clustering$;
+
+    -- This placeholder will be replaced by Chichi:
+    {{ users_sync_queries }}
+
+$$;
