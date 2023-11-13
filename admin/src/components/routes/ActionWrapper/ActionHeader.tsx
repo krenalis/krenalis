krenalis/krenalis

commit 4b3193b496a4b9576f5b34860cb4bb20dc22964a

admin: pass `onClose` directly to `ActionHeader`

admin/src/components/routes/ActionWrapper/ActionHeader.tsx
@@ -5,7 +5,11 @@ import SlInput from '@shoelace-style/shoelace/dist/react/input/index.js';
 import SlButton from '@shoelace-style/shoelace/dist/react/button/index.js';
 import SlIconButton from '@shoelace-style/shoelace/dist/react/icon-button/index.js';
 
-const ActionHeader = () => {
+interface ActionHeaderProps {
+	onClose: () => void;
+}
+
+const ActionHeader = ({ onClose }: ActionHeaderProps) => {
 	const [isNameEditable, setIsNameEditable] = useState(false);
 
 	const {
@@ -14,7 +18,6 @@ const ActionHeader = () => {
 		actionType,
 		setAction,
 		saveAction,
-		onClose,
 		isMappingSectionDisabled,
 		isEditing,
 		isSaveButtonLoading,
@@ -29,6 +32,7 @@ const ActionHeader = () => {
 
 	const onSave = async () => {
 		await saveAction();
+		onClose();
 	};
 
 	return (

admin/src/hooks/useActionData.ts
@@ -18,9 +18,9 @@ import { Action, ActionToSet, ActionType, Mapping, MappingExpression, Transforma
 import { ActionSchemasResponse, ExecQueryResponse, RecordsResponse } from '../types/external/api';
 import { ObjectType } from '../types/external/types';
 import Workspace from '../types/external/workspace';
+import { sleep } from '../lib/utils/sleep';
 
 const useActionData = (
-	onClose: () => void,
 	connection: TransformedConnection,
 	providedActionType: ActionType,
 	providedAction: Action,
@@ -67,7 +67,6 @@ const useActionData = (
 					actionType.EventType,
 				);
 			} catch (err) {
-				onClose();
 				if (err instanceof UnprocessableError) {
 					switch (err.code) {
 						case 'NoUsersSchema':
@@ -379,10 +378,8 @@ const useActionData = (
 
 		sessionStorage.setItem('newActionID', String(id));
 		setIsSaveButtonLoading(true);
-		setTimeout(() => {
-			setIsSaveButtonLoading(false);
-			onClose();
-		}, 200);
+		await sleep(200);
+		setIsSaveButtonLoading(false);
 	};
 
 	let mustComputeSchema: boolean = false;

admin/src/components/routes/ActionWrapper/Action.tsx
@@ -48,7 +48,7 @@ const Action = ({ actionType: providedActionType, action: providedAction }) => {
 		isMappingSectionDisabled,
 		disabledReason,
 		mustComputeSchema,
-	} = useActionData(onClose, connection, providedActionType, providedAction, setIsSaveButtonLoading, workspace);
+	} = useActionData(connection, providedActionType, providedAction, setIsSaveButtonLoading, workspace);
 
 	if (isLoading) {
 		return (
@@ -94,7 +94,7 @@ const Action = ({ actionType: providedActionType, action: providedAction }) => {
 			}}
 		>
 			<div className='action'>
-				<ActionHeader />
+				<ActionHeader onClose={onClose} />
 				<div className='body'>
 					{actionType!.Fields.includes('Filter') && <ActionFilters />}
 					{actionType!.Fields.includes('Query') && <ActionQuery />}

commit 8040860ddbbace4be9be7f98b5cb9c608a8f5abf

admin: do not close the action screen if saving returns an error
Commit 4b3193b introduced a new issue: now if saving the action returns
an error, the action screen is closed as if in reality the operation had
been successful.

This commit fixes the issue by avoiding the closure of the screen and by
showing the error messages, as it was before the erroneous commit.

admin/src/components/routes/ActionWrapper/ActionHeader.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import ActionContext from '../../../context/ActionContext';
+import AppContext from '../../../context/AppContext';
 import getConnectorLogo from '../../helpers/getConnectorLogo';
 import SlInput from '@shoelace-style/shoelace/dist/react/input/index.js';
 import SlButton from '@shoelace-style/shoelace/dist/react/button/index.js';
@@ -11,6 +12,8 @@ interface ActionHeaderProps {
 
 const ActionHeader = ({ onClose }: ActionHeaderProps) => {
 	const [isNameEditable, setIsNameEditable] = useState(false);
+
+	const { showError } = useContext(AppContext);
 
 	const {
 		connection,
@@ -31,8 +34,12 @@ const ActionHeader = ({ onClose }: ActionHeaderProps) => {
 	};
 
 	const onSave = async () => {
-		await saveAction();
-		onClose();
+		const err = await saveAction();
+		if (err == null) {
+			onClose();
+		} else {
+			showError(err);
+		}
 	};
 
 	return (

admin/src/context/ActionContext.ts
@@ -8,7 +8,7 @@ interface ActionContext {
 	connection: TransformedConnection;
 	action: TransformedAction;
 	setAction: React.Dispatch<React.SetStateAction<TransformedAction | undefined>>;
-	saveAction: () => Promise<void>;
+	saveAction: () => Promise<string | Error | null>;
 	actionType: TransformedActionType;
 	setActionType: React.Dispatch<React.SetStateAction<TransformedActionType | undefined>>;
 	isEditing: boolean;

admin/src/hooks/useActionData.ts
@@ -215,7 +215,7 @@ const useActionData = (
 
 	const saveAction = async () => {
 		if (action == null || actionType == null) {
-			return;
+			return 'Invalid action or action type';
 		}
 
 		let mapping: Mapping;
@@ -240,8 +240,7 @@ const useActionData = (
 					continue;
 				}
 				if (v.error && v.error !== '') {
-					showError(`Please fix the errors in the mapping before saving`);
-					return;
+					return `Please fix the errors in the mapping before saving`;
 				}
 				const property = flattenedOutputSchema![k];
 				const fullProperty = property.full;
@@ -263,8 +262,7 @@ const useActionData = (
 			try {
 				inputProperties = await api.expressionsProperties(expressions, actionType.InputSchema);
 			} catch (err) {
-				showError(err);
-				return;
+				return err;
 			}
 			for (const prop of inputProperties) {
 				const parentName = prop.split('.')[0];
@@ -330,14 +328,14 @@ const useActionData = (
 				);
 			}
 		} catch (err) {
-			showError(err);
-			return;
+			return err;
 		}
 
 		sessionStorage.setItem('newActionID', String(id));
 		setIsSaveButtonLoading(true);
 		await sleep(200);
 		setIsSaveButtonLoading(false);
+		return null;
 	};
 
 	const isTransformationAllowed: boolean = useMemo(