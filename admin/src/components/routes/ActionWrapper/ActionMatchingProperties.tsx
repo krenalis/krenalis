krenalis/krenalis

commit 8050c2969ee84e88b8d90c9f2cadf9f8a3cc923e

admin: add workaround to show matching properties in the action screen
Currently, when opening the editing screen of an already created action
that possesses matching properties, the fields of the matching
properties are populated with the value `undefined`.

The issue arises because the server serializes the data structure of
matching properties inconsistently compared to all other cases where it
sends properties to the UI. Specifically, the `name` field, which
usually contains the property name, is serialized as `Name` in this
case. Consequently, accessing `name` results in obtaining the value
`undefined`.

This commit introduces a temporary workaround by converting the property
type to `any` with a type assertion. This allows access to the `Name`
field and correctly displays the names of the properties without
triggering TypeScript errors.

This code should be removed once the server behavior is fixed.

Refer #381

admin/src/components/routes/ActionWrapper/ActionMatchingProperties.tsx
@@ -41,6 +41,14 @@ const ActionMatchingProperties = () => {
 		setAction(a);
 	};
 
+	// TODO: remove this when pull request #379 is closed.
+	//
+	// Here, a type conversion to 'any' is necessary, as the server serializes
+	// this data structure inconsistently compared to all other use cases. This
+	// conversion can be removed once pull request #379 is closed.
+	const internalMatchingProperty = action.MatchingProperties!.Internal as any;
+	const externalMatchingProperty = action.MatchingProperties!.External as any;
+
 	return (
 		<Section
 			title={`Matching properties`}
@@ -51,7 +59,7 @@ const ActionMatchingProperties = () => {
 				<ComboBoxInput
 					comboBoxListRef={internalMatchingPropertyListRef}
 					onInput={onUpdateMatchingProperties}
-					value={action.MatchingProperties!.Internal ? action.MatchingProperties!.Internal.name : ''}
+					value={internalMatchingProperty ? internalMatchingProperty.Name : ''}
 					label='Golden record property'
 					data-type='Internal'
 					className='inputProperty'
@@ -66,7 +74,7 @@ const ActionMatchingProperties = () => {
 					comboBoxListRef={externalMatchingPropertyListRef}
 					onInput={onUpdateMatchingProperties}
 					label={`${connection.name}'s property`}
-					value={action.MatchingProperties!.External ? action.MatchingProperties!.External.name : ''}
+					value={externalMatchingProperty ? externalMatchingProperty.Name : ''}
 					data-type='External'
 				></ComboBoxInput>
 				<ComboBoxList

commit c2aaba9c0bc86b88371bd46661ffb13823a80d57

admin: remove workaround for matching properties
Since the PR #379 has been merged into the main branch and the issue #381
has been resolved, this commit removes the workaround introduced by
commit 8050c2969ee84e88b8d90c9f2cadf9f8a3cc923e, restoring the
functionality of the interface for exporting users to apps.

Co-authored-by: Andrea Retini <retini@open2b.com>

admin/src/components/routes/ActionWrapper/ActionMatchingProperties.tsx
@@ -41,14 +41,6 @@ const ActionMatchingProperties = () => {
 		setAction(a);
 	};
 
-	// TODO: remove this when pull request #379 is closed.
-	//
-	// Here, a type conversion to 'any' is necessary, as the server serializes
-	// this data structure inconsistently compared to all other use cases. This
-	// conversion can be removed once pull request #379 is closed.
-	const internalMatchingProperty = action.MatchingProperties!.Internal as any;
-	const externalMatchingProperty = action.MatchingProperties!.External as any;
-
 	return (
 		<Section
 			title={`Matching properties`}
@@ -59,7 +51,7 @@ const ActionMatchingProperties = () => {
 				<ComboBoxInput
 					comboBoxListRef={internalMatchingPropertyListRef}
 					onInput={onUpdateMatchingProperties}
-					value={internalMatchingProperty ? internalMatchingProperty.Name : ''}
+					value={action.MatchingProperties!.Internal ? action.MatchingProperties!.Internal.name : ''}
 					label='Golden record property'
 					data-type='Internal'
 					className='inputProperty'
@@ -74,7 +66,7 @@ const ActionMatchingProperties = () => {
 					comboBoxListRef={externalMatchingPropertyListRef}
 					onInput={onUpdateMatchingProperties}
 					label={`${connection.name}'s property`}
-					value={externalMatchingProperty ? externalMatchingProperty.Name : ''}
+					value={action.MatchingProperties!.External ? action.MatchingProperties!.External.name : ''}
 					data-type='External'
 				></ComboBoxInput>
 				<ComboBoxList