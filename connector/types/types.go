krenalis/krenalis

commit e458640ae2c075a4e31f0c4106ef9745e575d8b4

connector/types: ignore ordering of enum values a workaround for #580
This commit introduces a temporary workaround for the issue #580.

See that issue for more details about this commit.

connector/types/types.go
@@ -1180,7 +1180,15 @@ func (t Type) EqualTo(t2 Type) bool {
 		return true
 	case []string:
 		vl2, ok := t2.vl.([]string)
-		return ok && slices.Equal(vl1, vl2)
+		if !ok || len(vl1) != len(vl2) {
+			return false
+		}
+		for _, v := range vl2 {
+			if !slices.Contains(vl1, v) {
+				return false
+			}
+		}
+		return true
 	case *regexp.Regexp:
 		vl2, ok := t2.vl.(*regexp.Regexp)
 		return ok && vl1.String() == vl2.String()

commit ca4fba4ea7147c426b9d9f91a8dfb0bd957fccdd

connector/types: revert workaround on comparison of enum ordering
Since the support for enums has been dropped in our PostgreSQL driver
(see the commit edf744c0113cacba86580fa1a21954e46ab833cf and the issue #576),
this commit reverts the workaround introduced in e458640ae2c075a4e31f0c4106ef9745e575d8b4,
as it is no longer necessary.

Close #580.

connector/types/types.go
@@ -1179,15 +1179,7 @@ func (t Type) EqualTo(t2 Type) bool {
 		return true
 	case []string:
 		vl2, ok := t2.vl.([]string)
-		if !ok || len(vl1) != len(vl2) {
-			return false
-		}
-		for _, v := range vl2 {
-			if !slices.Contains(vl1, v) {
-				return false
-			}
-		}
-		return true
+		return ok && slices.Equal(vl1, vl2)
 	case *regexp.Regexp:
 		vl2, ok := t2.vl.(*regexp.Regexp)
 		return ok && vl1.String() == vl2.String()