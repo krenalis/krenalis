krenalis/krenalis

commit 24dcaa79ea8e222ec3bc320148db89bd7b0c1f1d

all: move file actions on storage connections and do contour changes
This commit moves the actions on files on the Storage connections,
eliminating therefore the concept of File connections.

See the issue #563 for more details.

For #563.

Co-authored-by: Andrea Retini <retini@open2b.com>

apis/workspace.go
@@ -67,7 +67,6 @@ var (
 // It returns an errors.UnprocessableError error with code
 //   - ConnectorNotExist, if the connector does not exist.
 //   - InvalidSettings, if the settings are not valid.
-//   - StorageNotExist, if the storage does not exist.
 func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionToAdd, oAuthToken string) (int, error) {
 
 	this.apis.mustBeOpen()
@@ -81,17 +80,7 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 	if utf8.RuneCountInString(connection.Name) > 100 {
 		return 0, errors.BadRequest("name %q is not valid", connection.Name)
 	}
-	if connection.Storage < 0 || connection.Storage > maxInt32 {
-		return 0, errors.BadRequest("storage identifier %d is not valid", connection.Storage)
-	}
-	switch connection.Compression {
-	case NoCompression, ZipCompression, GzipCompression, SnappyCompression:
-	default:
-		return 0, errors.BadRequest("compression %q is not valid", connection.Compression)
-	}
-	if connection.Storage == 0 && connection.Compression != NoCompression {
-		return 0, errors.BadRequest("compression requires a storage")
-	}
+
 	if s := connection.Strategy; s != nil {
 		if !isValidStrategy(*s) {
 			return 0, errors.BadRequest("strategy %q is not valid", *s)
@@ -105,6 +94,9 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 	if !ok {
 		return 0, errors.Unprocessable(ConnectorNotExist, "connector %d does not exist", connection.Connector)
 	}
+	if c.Type == state.FileType {
+		return 0, errors.BadRequest("cannot add a connection with type File")
+	}
 
 	n := state.AddConnection{
 		Workspace:   this.workspace.ID,
@@ -112,35 +104,9 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 		Role:        state.Role(connection.Role),
 		Enabled:     connection.Enabled,
 		Connector:   connection.Connector,
-		Storage:     connection.Storage,
-		Compression: state.Compression(connection.Compression),
 		Strategy:    (*state.Strategy)(connection.Strategy),
 		WebsiteHost: connection.WebsiteHost,
 		BusinessID:  state.BusinessID(connection.BusinessID),
-	}
-	if n.Name == "" {
-		n.Name = c.Name
-	}
-
-	// Validate the storage.
-	if n.Storage > 0 {
-		if c.Type != state.FileType {
-			return 0, errors.BadRequest("connector %d cannot have a storage, it's a %s",
-				c.ID, strings.ToLower(c.Type.String()))
-		}
-		s, ok := this.workspace.Connection(n.Storage)
-		if !ok {
-			return 0, errors.Unprocessable(StorageNotExist, "storage %d does not exist", n.Storage)
-		}
-		if s.Connector().Type != state.StorageType {
-			return 0, errors.BadRequest("connection %d is not a storage", n.Storage)
-		}
-		if Role(s.Role) != connection.Role {
-			if connection.Role == Source {
-				return 0, errors.BadRequest("storage %d is not a source", n.Storage)
-			}
-			return 0, errors.BadRequest("storage %d is not a destination", n.Storage)
-		}
 	}
 
 	// Validate the strategy.
@@ -289,11 +255,11 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 		}
 		// Insert the connection.
 		_, err = tx.Exec(ctx, "INSERT INTO connections "+
-			"(id, workspace, name, type, role, enabled, connector, storage,"+
-			" compression, resource, strategy, website_host, business_id_name, business_id_label, settings)"+
-			" VALUES ($1, $2, $3, $4, $5, $6, $7, NULLIF($8, 0), $9, $10, $11, $12, $13, $14, $15)",
+			"(id, workspace, name, type, role, enabled, connector,"+
+			" resource, strategy, website_host, business_id_name, business_id_label, settings)"+
+			" VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
 			n.ID, n.Workspace, n.Name, c.Type, n.Role, n.Enabled, n.Connector,
-			n.Storage, n.Compression, n.Resource.ID, n.Strategy, n.WebsiteHost, n.BusinessID.Name,
+			n.Resource.ID, n.Strategy, n.WebsiteHost, n.BusinessID.Name,
 			n.BusinessID.Label, string(n.Settings))
 		if err != nil {
 			if postgres.IsForeignKeyViolation(err) {
@@ -302,8 +268,6 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 					err = errors.Unprocessable(WorkspaceNotExist, "workspace %d does not exist", n.Workspace)
 				case "connections_connector_fkey":
 					err = errors.Unprocessable(ConnectorNotExist, "connector %d does not exist", n.Connector)
-				case "connections_storage_fkey":
-					err = errors.Unprocessable(StorageNotExist, "storage %d does not exist", n.Storage)
 				}
 			}
 			return err
@@ -613,7 +577,6 @@ func (this *Workspace) Connection(ctx context.Context, id int) (*Connection, err
 		Role:         Role(c.Role),
 		Enabled:      c.Enabled,
 		Connector:    conn.ID,
-		Compression:  Compression(c.Compression),
 		Strategy:     (*Strategy)(c.Strategy),
 		WebsiteHost:  c.WebsiteHost,
 		BusinessID:   BusinessID(c.BusinessID),
@@ -621,10 +584,7 @@ func (this *Workspace) Connection(ctx context.Context, id int) (*Connection, err
 		ActionsCount: len(c.Actions()),
 		Health:       Health(c.Health),
 	}
-	// Set the storage.
-	if s, ok := c.Storage(); ok {
-		connection.Storage = s.ID
-	}
+
 	// Set the action types.
 	ts, err := connection.actionTypes(ctx)
 	if err != nil {
@@ -658,16 +618,12 @@ func (this *Workspace) Connections() []*Connection {
 			Role:         Role(c.Role),
 			Enabled:      c.Enabled,
 			Connector:    conn.ID,
-			Compression:  Compression(c.Compression),
 			Strategy:     (*Strategy)(c.Strategy),
 			WebsiteHost:  c.WebsiteHost,
 			BusinessID:   BusinessID(c.BusinessID),
 			HasSettings:  conn.HasSettings,
 			ActionsCount: len(c.Actions()),
 			Health:       Health(c.Health),
-		}
-		if s, ok := c.Storage(); ok {
-			connection.Storage = s.ID
 		}
 		infos[i] = &connection
 	}
@@ -1448,14 +1404,6 @@ type ConnectionToAdd struct {
 	// Connector is the identifier of the connector.
 	Connector int
 
-	// Storage is the identifier of the storage of a file connection.
-	// It must be 0 if the connection is not a file or has no storage.
-	Storage int
-
-	// Compression is the compression for file connections. It must be
-	// NoCompression if there is no storage.
-	Compression Compression
-
 	// Strategy is the strategy that determines how to merge anonymous and
 	// non-anonymous users. It must be nil for destination connections and
 	// non-event source connections.
@@ -1650,7 +1598,7 @@ func (this *Workspace) userIdentities(ctx context.Context, where expr.Expr, firs
 				if extIDLabel == "" {
 					extIDLabel = "ID"
 				}
-			case state.DatabaseType, state.FileType:
+			case state.DatabaseType, state.StorageType:
 				extIDLabel = "ID"
 			case state.MobileType, state.ServerType, state.WebsiteType:
 				extIDLabel = "User ID"

admin/src/components/routes/ActionWrapper/Action.css
@@ -108,6 +108,10 @@
 	margin-bottom: 0;
 }
 
+.action .section ::part(form-control-label) {
+	font-weight: 400;
+}
+
 .action .nameInput {
 	margin: 10px 25px;
 }
@@ -228,7 +232,6 @@
 
 .action .specialProperties .identityColumn .label,
 .action .specialProperties .timestampColumn .label {
-	font-weight: 600;
 	font-size: 13px;
 }
 
@@ -982,9 +985,37 @@ inherit the disabled style */
 	gap: 15px;
 }
 
+.action .action-file {
+	padding-bottom: 30px;
+}
+
+.action .action-file__file-connector,
+.action .action-file__compression {
+	margin-bottom: 30px;
+}
+
+.action .action-file__file-connector-logo {
+	margin-right: 8px;
+}
+
+.action .action-file__file-connector sl-option::part(label) {
+	display: flex;
+	align-items: center;
+	gap: 8px;
+}
+
+.action .action-file__file-connector-logo .littleLogo svg,
+.action .action-file__file-connector sl-option .littleLogo svg {
+	height: 18px;
+}
+
 .action .pathInputWrapper {
 	position: relative;
-	margin-bottom: 20px;
+	margin-bottom: 30px;
+}
+
+.action .pathInputLabelDescription {
+	font-size: 13px;
 }
 
 .action .completePath,
@@ -993,14 +1024,14 @@ inherit the disabled style */
 	left: 5px;
 	font-family: monospace;
 	font-size: 13px;
-	margin-top: 8px;
+	margin-top: 1px;
 	opacity: 0;
 	transition: opacity 0.3s;
 }
 
 .action .completePath.visible,
 .action .completePathError.visible {
-	opacity: 1;
+	opacity: 0.7;
 }
 
 .action .completePathError {
@@ -1012,10 +1043,10 @@ inherit the disabled style */
 	align-items: end;
 	justify-content: space-between;
 	gap: 20px;
+	margin-bottom: 30px;
 }
 
 .action .sheetsSelect {
-	margin-top: 20px;
 	width: 100%;
 }
 
@@ -1032,6 +1063,10 @@ inherit the disabled style */
 	color: var(--sl-color-danger-500);
 	margin-top: 3px;
 	font-weight: 600;
+}
+
+.action .sheetsInput {
+	margin-bottom: 30px;
 }
 
 .action .setSchemaFirst {

admin/src/components/routes/ActionWrapper/Action.tsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useRef, ReactNode } from 'react';
 import './Action.css';
 import ActionHeader from './ActionHeader';
 import ActionMapping from './ActionMapping';
-import ActionPath from './ActionPath';
+import ActionFile from './ActionFile';
 import ActionQuery from './ActionQuery';
 import ActionFilters from './ActionFilters';
 import ActionExportMode from './ActionExportMode';
@@ -41,6 +41,9 @@ const Action = ({ actionType: providedActionType, action: providedAction }) => {
 		isSaveHidden,
 		setIsSaveHidden,
 		setIsFileChanged,
+		setIsFileConnectorLoading,
+		isFileConnectorLoading,
+		setIsFileConnectorChanged,
 		setIsTableChanged,
 		setIsQueryChanged,
 		isMappingHidden,
@@ -86,6 +89,9 @@ const Action = ({ actionType: providedActionType, action: providedAction }) => {
 				isSaveButtonLoading,
 				setIsQueryChanged,
 				setIsFileChanged,
+				setIsFileConnectorLoading,
+				isFileConnectorLoading,
+				setIsFileConnectorChanged,
 				setIsTableChanged,
 				isSaveHidden,
 				setIsSaveHidden,
@@ -96,7 +102,7 @@ const Action = ({ actionType: providedActionType, action: providedAction }) => {
 				<div className='body'>
 					{actionType!.Fields.includes('Filter') && <ActionFilters />}
 					{actionType!.Fields.includes('Query') && <ActionQuery />}
-					{actionType!.Fields.includes('Path') && <ActionPath />}
+					{actionType!.Fields.includes('File') && <ActionFile />}
 					{actionType!.Fields.includes('Table') && <ActionTable />}
 					{actionType!.Fields.includes('ExportMode') && <ActionExportMode />}
 					{actionType!.Fields.includes('MatchingProperties') && <ActionMatchingProperties />}

admin/src/components/routes/ActionWrapper/ActionFile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
+import React, { useState, useContext, useEffect, useRef, useMemo, ReactNode } from 'react';
 import Section from '../../shared/Section/Section';
 import FeedbackButton from '../../shared/FeedbackButton/FeedbackButton';
 import Grid from '../../shared/Grid/Grid';
@@ -16,11 +16,247 @@ import SlSpinner from '@shoelace-style/shoelace/dist/react/spinner/index.js';
 import SlSelect from '@shoelace-style/shoelace/dist/react/select/index.js';
 import SlOption from '@shoelace-style/shoelace/dist/react/option/index.js';
 import SlDrawer from '@shoelace-style/shoelace/dist/react/drawer/index.js';
-
-import { CompletePathResponse, RecordsResponse, SheetsResponse } from '../../../types/external/api';
+import { CompletePathResponse, RecordsResponse, SheetsResponse, UIResponse } from '../../../types/external/api';
 import { GridColumn, GridRow } from '../../../types/componentTypes/Grid.types';
+import TransformedConnector from '../../../lib/helpers/transformedConnector';
+import ConnectorFieldInterface from '../../../types/external/ui';
+import { redirect } from 'react-router-dom';
+import { ConnectionContext } from '../../../context/providers/ConnectionProvider';
+import ConnectorField from '../../shared/ConnectorFields/ConnectorField';
+import SettingsForm from '../../shared/SettingsForm/SettingsForm';
+import LittleLogo from '../../shared/LittleLogo/LittleLogo';
+import actionContext from '../../../context/ActionContext';
+import { flattenSchema } from '../../../lib/helpers/transformedAction';
 
-const ActionPath = () => {
+const ActionFile = () => {
+	const [fileFields, setFileFields] = useState<ConnectorFieldInterface[]>([]);
+
+	const { connectors, api, selectedWorkspace, showStatus, handleError } = useContext(AppContext);
+	const { connection } = useContext(ConnectionContext);
+	const {
+		action,
+		setAction,
+		setIsFileChanged,
+		isFileConnectorLoading,
+		setIsFileConnectorLoading,
+		setIsFileConnectorChanged,
+		actionType,
+		isEditing,
+	} = useContext(actionContext);
+
+	const fileConnectorRef = useRef<number>(action.Connector);
+
+	useEffect(() => {
+		// check if the file connector id has been passed in the query
+		// parameters.
+		const f = new URL(document.location.href).searchParams.get('fileConnector');
+		if (f != null) {
+			const id = Number(f);
+			fileConnectorRef.current = id;
+			const connector = connectors.find((c) => c.id === id);
+			const a = { ...action };
+			a.Connector = id;
+			a.Sheet = connector.hasSheets ? '' : null;
+			setIsFileConnectorLoading(true);
+			setAction(a);
+		}
+	}, []);
+
+	useEffect(() => {
+		const fetchFields = async () => {
+			const a = { ...action };
+			const connector = connectors.find((c) => c.id === action.Connector);
+			if (connector.hasSettings === false) {
+				setFileFields([]);
+				setTimeout(() => setIsFileConnectorLoading(false), 300);
+				return;
+			}
+
+			let ui: UIResponse;
+			if (isEditing) {
+				try {
+					ui = await api.workspaces.connections.actionUiEvent(connection.id, a.ID, 'load', null);
+				} catch (err) {
+					setTimeout(() => setIsFileConnectorLoading(false), 300);
+					if (err instanceof NotFoundError) {
+						redirect('connectors');
+						showStatus(statuses.connectorDoesNotExistAnymore);
+						return;
+					}
+					if (err instanceof UnprocessableError) {
+						if (err.code === 'EventNotExist') {
+							handleError(
+								'An unexpected error has occurred. Please contact the administrator for more information.',
+							);
+						}
+						return;
+					}
+					handleError(err);
+					return;
+				}
+			} else {
+				try {
+					ui = await api.connectors.ui(selectedWorkspace, connector.id, connection.role, null);
+				} catch (err) {
+					setTimeout(() => setIsFileConnectorLoading(false), 300);
+					if (err instanceof NotFoundError) {
+						redirect('connectors');
+						showStatus(statuses.connectorDoesNotExistAnymore);
+						return;
+					}
+					if (err instanceof UnprocessableError) {
+						if (err.code === 'EventNotExist') {
+							console.error(
+								`Unprocessable: connector does not implement the 'load' event in its ServeUI method`,
+							);
+							handleError(
+								'An unexpected error has occurred. Please contact the administrator for more information.',
+							);
+						}
+						return;
+					}
+					handleError(err);
+					return;
+				}
+			}
+
+			setFileFields(ui.Form.Fields);
+			a.Settings = ui.Form.Values;
+			setAction(a);
+			setTimeout(() => setIsFileConnectorLoading(false), 300);
+		};
+
+		if (action.Connector == 0) {
+			return;
+		}
+		fetchFields();
+	}, [fileConnectorRef.current]);
+
+	const { hasSheets, icon, fileExtension } = useMemo(() => {
+		const connector = connectors.find((c) => c.id === action.Connector);
+		return { hasSheets: connector?.hasSheets, icon: connector?.icon, fileExtension: connector?.fileExtension };
+	}, [action]);
+
+	const onFileConnectorChange = (e) => {
+		const id = Number(e.target.value);
+		fileConnectorRef.current = id;
+		const connector = connectors.find((c) => c.id === id);
+		const a = { ...action };
+		// reset the action.
+		a.Connector = id;
+		a.Compression = '';
+		a.Settings = null;
+		a.Sheet = connector.hasSheets ? '' : null;
+		a.Path = '';
+		a.IdentityColumn = '';
+		a.TimestampColumn = '';
+		a.TimestampFormat = '';
+		a.Transformation.Mapping = flattenSchema(actionType.OutputSchema);
+		a.Transformation.Function = null;
+		setIsFileConnectorLoading(true);
+		setIsFileConnectorChanged(true);
+		setAction(a);
+	};
+
+	const onCompressionChange = (e) => {
+		const a = { ...action };
+		a.Compression = e.target.value;
+		setIsFileChanged(true);
+		setAction(a);
+	};
+
+	const onFieldChange = (name: string, value: any) => {
+		const a = { ...action };
+		setIsFileChanged(true);
+		a.Settings = { ...a.Settings, [name]: value };
+		setAction(a);
+	};
+
+	const fileConnectors: TransformedConnector[] = [];
+	for (const c of connectors) {
+		if (c.isFile) {
+			fileConnectors.push(c);
+		}
+	}
+
+	const fieldsToRender: ReactNode[] = [];
+	for (const f of fileFields) {
+		fieldsToRender.push(<ConnectorField key={f.Label} field={f} />);
+	}
+
+	return (
+		<Section title={`File`} className='action-file' description='The settings of the file' padded>
+			<SlSelect
+				label='Type'
+				className='action-file__file-connector'
+				value={String(action.Connector)}
+				onSlChange={onFileConnectorChange}
+			>
+				{action.Connector !== 0 && (
+					<div className='action-file__file-connector-logo' slot='prefix'>
+						<LittleLogo icon={icon} />
+					</div>
+				)}
+				{fileConnectors.map((f) => (
+					<SlOption key={f.id} value={String(f.id)}>
+						<div slot='prefix'>
+							<LittleLogo icon={f.icon} />
+						</div>
+						{f.name}
+					</SlOption>
+				))}
+			</SlSelect>
+			{isFileConnectorLoading ? (
+				<SlSpinner
+					style={
+						{
+							display: 'block',
+							position: 'relative',
+							top: '50px',
+							margin: 'auto',
+							fontSize: '3rem',
+							'--track-width': '6px',
+						} as React.CSSProperties
+					}
+				></SlSpinner>
+			) : (
+				action.Connector !== 0 && (
+					<div className='action-file__file-settings'>
+						<FileSettings hasSheets={hasSheets} fileExtension={fileExtension}>
+							<SlSelect
+								className='action-file__compression'
+								name='compression'
+								value={action.Compression}
+								label='Compression'
+								onSlChange={onCompressionChange}
+							>
+								<SlOption value=''>None</SlOption>
+								<SlOption value='Zip'>Zip</SlOption>
+								<SlOption value='Gzip'>Gzip</SlOption>
+								<SlOption value='Snappy'>Snappy</SlOption>
+							</SlSelect>
+							{fieldsToRender.length > 0 && (
+								<SettingsForm
+									fields={fieldsToRender}
+									values={action.Settings}
+									onChange={onFieldChange}
+								/>
+							)}
+						</FileSettings>
+					</div>
+				)
+			)}
+		</Section>
+	);
+};
+
+interface FileSettingsProps {
+	hasSheets: boolean;
+	fileExtension: string;
+	children: ReactNode;
+}
+
+const FileSettings = ({ hasSheets, fileExtension, children }: FileSettingsProps) => {
 	const [sheets, setSheets] = useState<string[]>([]);
 	const [areSheetsLoading, setAreSheetsLoading] = useState<boolean>(false);
 	const [hasSheetsError, setHasSheetsError] = useState<boolean>(false);
@@ -30,9 +266,18 @@ const ActionPath = () => {
 	const [filePreviewRows, setFilePreviewRows] = useState<GridRow[] | null>(null);
 	const [showFilePreviewContent, setShowFilePreviewContent] = useState<boolean>(false);
 
-	const { showStatus, handleError, api, setIsLoadingConnections } = useContext(AppContext);
-	const { connection, action, setAction, actionType, setActionType, isImport, mappingSectionRef, setIsFileChanged } =
-		useContext(ActionContext);
+	const { showStatus, handleError, api } = useContext(AppContext);
+	const {
+		connection,
+		action,
+		setAction,
+		actionType,
+		setActionType,
+		isImport,
+		mappingSectionRef,
+		setIsFileChanged,
+		setIsFileConnectorChanged,
+	} = useContext(ActionContext);
 
 	const getCompletePathTimeoutID = useRef<number>();
 	const sheetsSelectRef = useRef<any>();
@@ -65,7 +310,13 @@ const ActionPath = () => {
 			pathRef.current.lastSheetFetch = pathRef.current.lastUpdate;
 			let res: SheetsResponse;
 			try {
-				res = await api.workspaces.connections.sheets(connection.id, action.Path!);
+				res = await api.workspaces.connections.sheets(
+					connection.id,
+					action.Connector,
+					action.Path!,
+					action.Compression,
+					action.Settings,
+				);
 			} catch (err) {
 				if (err instanceof UnprocessableError || err instanceof BadRequestError) {
 					handleError(err.message);
@@ -77,7 +328,7 @@ const ActionPath = () => {
 			}
 			setSheets(res.sheets);
 		};
-		if (!connection.connector.hasSheets || action.Path == null || action.Path === '') {
+		if (!hasSheets || action.Path === '') {
 			return;
 		}
 		fetchSheets();
@@ -103,7 +354,7 @@ const ActionPath = () => {
 		setAction(a);
 		setCompletePath('');
 		setCompletePathError('');
-		if (path === '' || connection.storage === 0) {
+		if (path === '') {
 			return;
 		}
 		getCompletePathTimeoutID.current = window.setTimeout(async () => {
@@ -113,13 +364,6 @@ const ActionPath = () => {
 			} catch (err) {
 				if (err instanceof UnprocessableError && err.code === 'InvalidPath') {
 					setCompletePathError(err.message);
-					return;
-				}
-				if (err instanceof NotFoundError) {
-					showStatus(statuses.linkedStorageDoesNotExistAnymore);
-					const cn = { ...connection };
-					cn.storage = 0;
-					setIsLoadingConnections(true);
 					return;
 				}
 				handleError(err);
@@ -155,7 +399,13 @@ const ActionPath = () => {
 		pathRef.current.lastSheetFetch = pathRef.current.lastUpdate;
 		let res: SheetsResponse;
 		try {
-			res = await api.workspaces.connections.sheets(connection.id, action.Path!);
+			res = await api.workspaces.connections.sheets(
+				connection.id,
+				action.Connector,
+				action.Path!,
+				action.Compression,
+				action.Settings,
+			);
 		} catch (err) {
 			setTimeout(() => {
 				if (err instanceof UnprocessableError || err instanceof BadRequestError) {
@@ -192,11 +442,11 @@ const ActionPath = () => {
 	};
 
 	const onFilePreview = async () => {
-		if (actionType.Fields.includes('Path') && action.Path === '') {
+		if (action.Path === '') {
 			handleError('Please enter a path');
 			return;
 		}
-		if (actionType.Fields.includes('Sheet') && action.Sheet === '') {
+		if (hasSheets && action.Sheet === '') {
 			handleError('Please enter a sheet');
 			return;
 		}
@@ -234,11 +484,11 @@ const ActionPath = () => {
 	};
 
 	const onConfirmFile = async () => {
-		if (actionType.Fields.includes('Path') && action.Path === '') {
+		if (action.Path === '') {
 			handleError('Please enter a path');
 			return;
 		}
-		if (actionType.Fields.includes('Sheet') && action.Sheet === '') {
+		if (hasSheets && action.Sheet === '') {
 			handleError('Please enter a sheet');
 			return;
 		}
@@ -253,6 +503,7 @@ const ActionPath = () => {
 			const actionTyp = { ...actionType };
 			actionTyp.InputSchema = res.schema;
 			setActionType(actionTyp);
+			setIsFileConnectorChanged(false);
 			setTimeout(() => {
 				const top = mappingSectionRef.current!.getBoundingClientRect().top;
 				mappingSectionRef.current!.closest('.fullscreen').scrollBy({
@@ -269,8 +520,11 @@ const ActionPath = () => {
 		try {
 			res = await api.workspaces.connections.records(
 				connection.id,
+				action.Connector,
 				path,
 				sheet === undefined ? null : sheet,
+				action.Compression,
+				action.Settings,
 				limit,
 			);
 		} catch (err) {
@@ -278,9 +532,6 @@ const ActionPath = () => {
 				switch (err.code) {
 					case 'ReadFileFailed':
 						showStatus({ variant: variants.DANGER, icon: icons.INVALID_INSERTED_VALUE, text: err.message });
-						break;
-					case 'NoStorage':
-						showStatus(statuses.linkedStorageDoesNotExistAnymore);
 						break;
 					default:
 						handleError(err);
@@ -301,29 +552,32 @@ const ActionPath = () => {
 	};
 
 	return (
-		<Section
-			title={`Path${actionType.Fields.includes('Sheet') ? ' and Sheet' : ''}`}
-			description={`The path${actionType.Fields.includes('Sheet') ? ' and sheet' : ''} of the file.${
-				connection.role == 'Destination' ? ' You can use the ${now}, ${today} and ${unix} placeholders.' : ''
-			}`}
-			padded
-		>
+		<div>
 			<div className='pathInputWrapper'>
 				<SlInput
 					className='pathInput'
 					name='path'
 					value={action.Path!}
-					label={actionType.Fields.includes('Sheet') ? 'Path' : undefined}
 					type='text'
 					onSlInput={onUpdatePath}
-					placeholder={`${actionType.Target.toLowerCase()}.${connection.connector.fileExtension}`}
-				/>
+					placeholder={`${actionType.Target.toLowerCase()}.${fileExtension}`}
+				>
+					<div className='pathInputLabel' slot='label'>
+						<div className='pathInputLabelText'>Path</div>
+						<div className='pathInputLabelDescription'>
+							The path of the file.
+							{connection.role == 'Destination'
+								? ' You can use the ${now}, ${today} and ${unix} placeholders.'
+								: ''}
+						</div>
+					</div>
+				</SlInput>
 				<div className={`completePathError${completePathError !== '' ? ' visible' : ''}`}>
 					{completePathError}
 				</div>
 				<div className={`completePath${completePath !== '' ? ' visible' : ''}`}>{completePath}</div>
 			</div>
-			{actionType.Fields.includes('Sheet') &&
+			{hasSheets &&
 				(connection.role === 'Source' ? (
 					<div className='sheetsSelectWrapper'>
 						<SlSelect
@@ -370,6 +624,7 @@ const ActionPath = () => {
 						onSlInput={onUpdateSheet}
 					/>
 				))}
+			{children}
 			{isImport && (
 				<div className='fileButtons'>
 					<SlButton variant='neutral' size='small' onClick={onFilePreview}>
@@ -418,7 +673,7 @@ const ActionPath = () => {
 					></SlSpinner>
 				)}
 			</SlDrawer>
-		</Section>
+		</div>
 	);
 };
 
@@ -445,4 +700,4 @@ const fromColumnNameToNumber = (columnName: string): number => {
 	return num;
 };
 
-export default ActionPath;
+export default ActionFile;

admin/src/components/routes/ActionWrapper/ActionMapping.tsx
@@ -323,7 +323,7 @@ const ActionMapping = forwardRef<any>((_, ref) => {
 				padded={false}
 				className={mode}
 			>
-				{connection.isFile && connection.isSource && (
+				{connection.isStorage && connection.isSource && (
 					<div className='specialProperties'>
 						<div className='identityColumn'>
 							<div className='label'>
@@ -791,7 +791,7 @@ const FullscreenTransformation = ({
 				if (connection.isFile && connection.isSource) {
 					let res: RecordsResponse;
 					try {
-						res = await api.workspaces.connections.records(connection.id, action.Path, action.Sheet, 20);
+						// res = await api.workspaces.connections.records(connection.id, action.Path, action.Sheet, 20);
 					} catch (err) {
 						handleError(err);
 						return;

commit 4ac643925d1758996e973010a5caf5720be8f7ce

apis: fix connection name
This commit fixes the commit 24dcaa79ea8e222ec3bc320148db89bd7b0c1f1d,
which inadvertently removed an "if" statement.

apis/workspace.go
@@ -108,6 +108,9 @@ func (this *Workspace) AddConnection(ctx context.Context, connection ConnectionT
 		WebsiteHost: connection.WebsiteHost,
 		BusinessID:  connection.BusinessID,
 	}
+	if n.Name == "" {
+		n.Name = c.Name
+	}
 
 	// Validate the strategy.
 	if connection.Role == Source {