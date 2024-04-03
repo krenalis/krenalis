236737959609d741659aa88765e6408a5ce380ea
Gianluca Mondini <mondini@open2b.com>
2024-04-03 12:11:57 +0200

    apis/datastore/warehouses/postgresql: format SQL file

--- apis/datastore/warehouses/postgresql/tables/users_identities.sql
+++ apis/datastore/warehouses/postgresql/tables/users_identities.sql
@@ -1,12 +1,13 @@
 CREATE TABLE users_identities (
-    "_identity_id"   serial,
-    "_connection"    int NOT NULL DEFAULT 0,
-    "_external_id"   text NOT NULL DEFAULT '',
-    "_displayed_id"  varchar(40) NOT NULL DEFAULT '',
+    "_identity_id" serial,
+    "_connection" int NOT NULL DEFAULT 0,
+    "_external_id" text NOT NULL DEFAULT '',
+    "_displayed_id" varchar(40) NOT NULL DEFAULT '',
     "_anonymous_ids" text[],
-    "_updated_at"    timestamp NOT NULL,
-    "_gid"           int NOT NULL DEFAULT 0,
-    "__cluster__"       serial,
+    "_updated_at" timestamp NOT NULL,
+    "_gid" int NOT NULL DEFAULT 0,
+    
+    "__cluster__" serial,
 
     "email" varchar(300), -- TODO(Gianluca): see https://github.com/open2b/chichi/issues/628.
 
