krenalis/krenalis

commit 28f3051fb69f2f1c03c39234ed1d9345a1f7a737

assets: ensure 'Run' button imports users without reimporting all
This change ensures that when executing a user import through an action,
re-importation no longer takes place. Instead, only new or modified
users since the previous import are included.

assets/src/lib/api/api.ts
@@ -330,13 +330,12 @@ class Connections {
 		);
 	};
 
-	executeAction = async (connection: number, action: number, reimport: boolean): Promise<void> => {
+	executeAction = async (connection: number, action: number): Promise<void> => {
 		return await call(
 			`${this.apiURL}/connections/${encodeURIComponent(connection)}/actions/${encodeURIComponent(
 				action,
 			)}/executions`,
 			http.POST,
-			{ Reimport: reimport },
 		);
 	};
 

assets/src/components/routes/ConnectionActions/ActionsGrid.tsx
@@ -89,7 +89,7 @@ const ActionsGrid = ({ newActionID, actions, onSelectAction }: ActionsGridProps)
 			</div>
 		);
 		try {
-			await api.workspaces.connections.executeAction(connection.id, actionID, true); // TODO: handle the reimport bool.
+			await api.workspaces.connections.executeAction(connection.id, actionID);
 		} catch (err) {
 			if (err instanceof UnprocessableError) {
 				runButtonRefs.current[actionID].current!.error(

commit 7b00fc55eeac032c934656f2604a59f6d8edc944

assets: Fix malfunctioning 'Run' button for actions
The commit 28f3051 introduced a bug that caused the 'Run' button for
actions to malfunction. This commit addresses and resolves the issue.

assets/src/lib/api/api.ts
@@ -336,6 +336,7 @@ class Connections {
 				action,
 			)}/executions`,
 			http.POST,
+			{ Reimport: false },
 		);
 	};
 