7281aa9ad1dd8e975510b32f6f8585ac4bfe60a2
Gianluca Mondini <mondini@open2b.com>
2024-06-13 11:49:21 +0200

    apis/datastore: add last change time to default users view of PostgreSQL

--- apis/datastore/warehouses/postgresql/tables/users.sql
+++ apis/datastore/warehouses/postgresql/tables/users.sql
@@ -2,8 +2,13 @@ CREATE TABLE _users (
     "__id__" uuid,
     "__identities__" int [],
     "__last_change_time__" timestamp NOT NULL,
-    
     "email" varchar(300)
 );
 
-CREATE VIEW "users" AS SELECT "__id__", "email" FROM "_users";
+CREATE VIEW "users" AS
+SELECT
+    "__id__",
+    "__last_change_time__",
+    "email"
+FROM
+    "_users";
