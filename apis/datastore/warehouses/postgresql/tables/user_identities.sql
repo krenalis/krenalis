711dd762f48b21a7e36c685b25d244c97dd0733b
Marco Gazerro <gazerro@open2b.com>
2024-06-21 10:25:30 +0200

    apis/datastore/warehouses/postgresql: prefer `integer` to `int`
    Prefer 'integer' instead of 'int' when referring the integer type.

--- apis/datastore/warehouses/postgresql/tables/user_identities.sql
+++ apis/datastore/warehouses/postgresql/tables/user_identities.sql
@@ -1,9 +1,9 @@
 CREATE TABLE _user_identities (
     "__pk__" serial,
-    "__action__" int NOT NULL,
+    "__action__" integer NOT NULL,
     "__is_anonymous__" boolean NOT NULL DEFAULT FALSE,
     "__identity_id__" text NOT NULL,
-    "__connection__" int NOT NULL,
+    "__connection__" integer NOT NULL,
     "__anonymous_ids__" text [],
     "__last_change_time__" timestamp NOT NULL,
     "__gid__" uuid,
