69a9f78a960f71aabbf243a680cd08fb991c61d9
Gianluca Mondini <mondini@open2b.com>
2024-06-25 14:31:21 +0200

    apis: add initial tests on `validateActionToSet`

--- apis/action_validate_test.go
+++ apis/action_validate_test.go
@@ -8,11 +8,343 @@
 package apis
 
 import (
+	"context"
 	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/open2b/chichi/apis/state"
+	"github.com/open2b/chichi/apis/transformers"
 	"github.com/open2b/chichi/types"
 )
+
+func Test_validateActionToSet(t *testing.T) {
+
+	tests := []struct {
+		name string // test name
+		v    actionToValidate
+		err  string // empty string if no validation error is expected
+	}{
+
+		// Actions that are correct.
+
+		{
+			name: "Source app action that imports users with a mapping",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Import users",
+					InSchema: types.Object([]types.Property{
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Mapping: map[string]string{
+							"email_out": "email_in",
+						},
+					},
+				},
+				target:        state.Users,
+				role:          state.Source,
+				connectorType: state.AppType,
+			},
+		},
+		{
+			name: "Source app action that imports users with a transformation function",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Import users",
+					InSchema: types.Object([]types.Property{
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Function: &TransformationFunction{
+							Source: strings.Join([]string{
+								`def transform(user: dict) -> dict:`,
+								`    return {`,
+								`        "email_out": user["email_in"],`,
+								`    }`}, "\n"),
+							Language:      "Python",
+							InProperties:  []string{"email_in"},
+							OutProperties: []string{"email_out"},
+						},
+					},
+				},
+				target:        state.Users,
+				role:          state.Source,
+				connectorType: state.AppType,
+				provider:      testProvider{},
+			},
+		},
+		{
+			name: "Source database action that imports users with a mapping",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Import users",
+					InSchema: types.Object([]types.Property{
+						{Name: "id", Type: types.Int(32)},
+						{Name: "timestamp", Type: types.DateTime()},
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Mapping: map[string]string{
+							"email_out": "email_in",
+						},
+					},
+					Query:                  "SELECT id, timestamp, email_in FROM my_table",
+					IdentityProperty:       "id",
+					LastChangeTimeProperty: "timestamp",
+				},
+				target:        state.Users,
+				role:          state.Source,
+				connectorType: state.DatabaseType,
+			},
+		},
+
+		// TODO(Gianluca): there is a validation error that prevents this case
+		// from being valid. Investigate on this.
+
+		// {
+		// 	name: "Source file action that imports users with a mapping",
+		// 	v: actionToValidate{
+		// 		action: ActionToSet{
+		// 			Name: "Import users",
+		// 			InSchema: types.Object([]types.Property{
+		// 				{Name: "id", Type: types.Int(32)},
+		// 				{Name: "timestamp", Type: types.DateTime()},
+		// 				{Name: "email_in", Type: types.Text()},
+		// 			}),
+		// 			OutSchema: types.Object([]types.Property{
+		// 				{Name: "email_out", Type: types.Text()},
+		// 			}),
+		// 			Transformation: Transformation{
+		// 				Mapping: map[string]string{
+		// 					"email_out": "email_in",
+		// 				},
+		// 			},
+		// 			Path:                   "my_file.csv",
+		// 			IdentityProperty:       "id",
+		// 			LastChangeTimeProperty: "timestamp",
+		// 		},
+		// 		target:        state.Users,
+		// 		role:          state.Source,
+		// 		connectorType: state.FileStorageType,
+		// 		fileConnector: struct {
+		// 			name      string
+		// 			hasUI     bool
+		// 			hasSheets bool
+		// 		}{
+		// 			name:      "CSV",
+		// 			hasUI:     false,
+		// 			hasSheets: false,
+		// 		},
+		// 	},
+		// },
+
+		{
+			name: "Source website action that imports users with a mapping",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name:     "Import users",
+					InSchema: types.Type{},
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Mapping: map[string]string{
+							"email_out": "traits.email",
+						},
+					},
+				},
+				target:        state.Users,
+				role:          state.Source,
+				connectorType: state.WebsiteType,
+			},
+		},
+
+		{
+			name: "Source website action that imports events",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Import events",
+				},
+				target:        state.Events,
+				role:          state.Source,
+				connectorType: state.WebsiteType,
+			},
+		},
+
+		{
+			name: "Destination app action that exports users with a mapping",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Export users",
+					InSchema: types.Object([]types.Property{
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Mapping: map[string]string{
+							"email_out": "email_in",
+						},
+					},
+					ExportMode: &[]ExportMode{CreateOrUpdate}[0],
+					MatchingProperties: &MatchingProperties{
+						Internal: "email_in",
+						External: types.Property{Name: "email", Type: types.Text()},
+					},
+					ExportOnDuplicatedUsers: &[]bool{false}[0],
+				},
+				target:        state.Users,
+				role:          state.Destination,
+				connectorType: state.AppType,
+			},
+		},
+
+		{
+			name: "Destination app action that exports users with a transformation function",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Export users",
+					InSchema: types.Object([]types.Property{
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Function: &TransformationFunction{
+							Source: strings.Join([]string{
+								`def transform(user: dict) -> dict:`,
+								`    return {`,
+								`        "email_out": user["email_in"],`,
+								`    }`}, "\n"),
+							Language:      "Python",
+							InProperties:  []string{"email_in"},
+							OutProperties: []string{"email_out"},
+						},
+					},
+					ExportMode: &[]ExportMode{CreateOrUpdate}[0],
+					MatchingProperties: &MatchingProperties{
+						Internal: "email_in",
+						External: types.Property{Name: "email", Type: types.Text()},
+					},
+					ExportOnDuplicatedUsers: &[]bool{false}[0],
+				},
+				target:        state.Users,
+				role:          state.Destination,
+				connectorType: state.AppType,
+				provider:      testProvider{},
+			},
+		},
+
+		{
+			name: "Destination app action that dispatches events with a mapping",
+			v: actionToValidate{
+				action: ActionToSet{
+					Name: "Dispatch events to app",
+					// TODO(Gianluca): is this correct? Currently the validation
+					// of the action accepts it like this, but shouldn't this be
+					// the event schema (and therefore the invalid schema, at
+					// the API level), as it happens for the import of
+					// identities from events?
+					InSchema: types.Object([]types.Property{
+						{Name: "email_in", Type: types.Text()},
+					}),
+					OutSchema: types.Object([]types.Property{
+						{Name: "email_out", Type: types.Text()},
+					}),
+					Transformation: Transformation{
+						Mapping: map[string]string{
+							"email_out": "email_in",
+						},
+					},
+				},
+				target:        state.Events,
+				role:          state.Destination,
+				connectorType: state.AppType,
+			},
+		},
+
+		// Actions that are invalid.
+
+		// TODO(Gianluca): commented as it panics.
+		// {
+		// 	name: "Source app action that imports users with a mapping",
+		// 	v: actionToValidate{
+		// 		action: ActionToSet{
+		// 			Name: "Import users",
+		// 			InSchema: types.Object([]types.Property{
+		// 				{Name: "email_in", Type: types.Text()},
+		// 			}),
+		// 			OutSchema: types.Object([]types.Property{
+		// 				{Name: "email_out", Type: types.Text()},
+		// 			}),
+		// 			Transformation: Transformation{
+		// 				Mapping: map[string]string{
+		// 					"not_existent_property": "email_in",
+		// 				},
+		// 			},
+		// 		},
+		// 		target:        state.Users,
+		// 		role:          state.Source,
+		// 		connectorType: state.AppType,
+		// 	},
+		// },
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateActionToSet(test.v)
+			var gotErr string
+			if err != nil {
+				gotErr = err.Error()
+			}
+			if gotErr != test.err {
+				if gotErr == "" {
+					t.Fatalf("expected validation error %q, got no errors", test.err)
+				}
+				if test.err == "" {
+					t.Fatalf("no validation errors expected, got %q", gotErr)
+				}
+				t.Fatalf("expected validation error %q, got %q", test.err, gotErr)
+			}
+		})
+	}
+
+}
+
+// testProvider is a transformers.Provider which implements the minimum set of
+// functionalities to be used in the validateActionToSet tests.
+type testProvider struct{}
+
+var _ transformers.Provider = testProvider{}
+
+func (testProvider) Call(ctx context.Context, name, version string, inSchema, outSchema types.Type, values []map[string]any) ([]transformers.Result, error) {
+	panic("not implemented")
+}
+func (testProvider) Close(ctx context.Context) error { panic("not implemented") }
+func (testProvider) Create(ctx context.Context, name, source string) (string, error) {
+	panic("not implemented")
+}
+func (testProvider) Delete(ctx context.Context, name string) error {
+	panic("not implemented")
+}
+func (testProvider) SupportLanguage(language state.Language) bool {
+	return language == state.JavaScript || language == state.Python
+}
+func (testProvider) Update(ctx context.Context, name, source string) (string, error) {
+	panic("not implemented")
+}
 
 func Test_unusedProperties(t *testing.T) {
 	cases := []struct {
