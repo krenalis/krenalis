912b6ca22b290aaa5640937810981bb51067efe5
Marco Gazerro <gazerro@open2b.com>
2024-07-17 18:14:06 +0200

    types: fix incorrect error messages in `Test_AsRole` test
    Previously, error messages in the Test_AsRole test displayed objects
    without properties, resulting in unclear messages such as "expected
    schema Object != got Object". This change corrects the error messages to
    include the actual properties.

--- types/utils_test.go
+++ types/utils_test.go
@@ -165,10 +165,10 @@ func Test_AsRole(t *testing.T) {
 				t.Fatal("unexpected invalid schema")
 			}
 			if !expectedValid && gotValid {
-				t.Fatalf("expecting an invalid schema, got %v", got)
+				t.Fatalf("expecting an invalid schema, got %#v", got)
 			}
 			if !Equal(cas.expected, got) {
-				t.Fatalf("expected schema %v != got %v", cas.expected, got)
+				t.Fatalf("expected schema %#v != got %#v", cas.expected, got)
 			}
 		})
 	}
