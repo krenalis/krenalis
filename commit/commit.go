c85a8aeffef8a88a04aeba785f3e09532ca8bbca
Marco Gazerro <gazerro@open2b.com>
2024-11-21 16:04:55 +0100

    javascript-sdk: update Deno to v2.1.0 and update dependencies

--- commit/commit.go
+++ commit/commit.go
@@ -24,7 +24,7 @@ import (
 // expectedDenoVersion is the expected Deno version.
 //
 // Keep in sync with the version within ".github/workflows/main.yml".
-const expectedDenoVersion = "2.0.1"
+const expectedDenoVersion = "2.1.0"
 
 func main() {
 
