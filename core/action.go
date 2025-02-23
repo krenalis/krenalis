krenalis/krenalis

commit 0922e0d492ea5f6391f65ef568d71116fef2abe6

core: replace transformation function names with identifiers
This change introduces function identifiers returned by providers for
calling, updating, and deleting transformation functions, replacing
function names.

core/action.go
@@ -22,7 +22,6 @@ import (
 	"github.com/meergo/meergo/core/errors"
 	"github.com/meergo/meergo/core/events"
 	"github.com/meergo/meergo/core/state"
-	"github.com/meergo/meergo/core/transformers"
 	"github.com/meergo/meergo/core/transformers/mappings"
 	"github.com/meergo/meergo/core/util"
 	"github.com/meergo/meergo/json"
@@ -102,8 +101,8 @@ type Language string
 
 // TransformationFunction represents a transformation function.
 type TransformationFunction struct {
-	Source       string   `json:"source"` // Source cannot be longer than MaxFunctionSourceSize runes.
 	Language     Language `json:"language"`
+	Source       string   `json:"source"` // Source cannot be longer than MaxFunctionSourceSize runes.
 	PreserveJSON bool     `json:"preserveJSON"`
 	InPaths      []string `json:"inPaths"`
 	OutPaths     []string `json:"outPaths"`
@@ -720,63 +719,57 @@ func (this *Action) Update(ctx context.Context, action ActionToSet) error {
 
 	// Transformation.
 	if fn := n.Transformation.Function; fn != nil {
-		if this.action.Transformation.Function == nil {
-			name := util.TransformationFunctionName(n.ID, fn.Language)
-			version, err := this.core.transformerProvider.Create(ctx, name, fn.Source)
-			if err == transformers.ErrFunctionExist {
-				version, err = this.core.transformerProvider.Update(ctx, name, fn.Source)
-			}
+		current := this.action.Transformation.Function
+		if current == nil || current.Language != fn.Language {
+			name := util.TransformationFunctionName(n.ID)
+			fn.ID, fn.Version, err = this.core.transformerProvider.Create(ctx, name, fn.Language, fn.Source)
 			if err != nil {
 				return err
 			}
-			n.Transformation.Function.Version = version
-		} else if this.action.Transformation.Function.Source != fn.Source || this.action.Transformation.Function.Language != fn.Language {
-			name := util.TransformationFunctionName(n.ID, fn.Language)
-			version, err := this.core.transformerProvider.Update(ctx, name, fn.Source)
-			if err == transformers.ErrFunctionNotExist {
-				version, err = this.core.transformerProvider.Create(ctx, name, fn.Source)
-			}
+		} else if this.action.Transformation.Function.Source != fn.Source {
+			fn.ID = current.ID
+			fn.Version, err = this.core.transformerProvider.Update(ctx, current.ID, fn.Source)
 			if err != nil {
 				return err
 			}
-			n.Transformation.Function.Version = version
 		} else {
-			// The function's source code and language should not be changed.
+			// The function's language and source code should not be changed.
 			// It will be verified during the transaction and assigned the current version.
 		}
 	}
 
 	update := "UPDATE actions SET\n" +
 		"name = $1, enabled = $2, in_schema = $3, out_schema = $4, filter = $5, " +
-		"transformation_mapping = $6, transformation_source = $7, transformation_language = $8, " +
-		"transformation_version = $9, transformation_preserve_json = $10, transformation_in_paths = $11, " +
-		"transformation_out_paths = $12, query = $13, format = $14, path = $15, sheet = $16, " +
-		"compression = $17, order_by = $18, format_settings = $19, export_mode = $20, matching_in = $21, " +
-		"matching_out = $22, update_on_duplicates = $23, table_name = $24, table_key = $25, " +
-		"identity_column = $26, last_change_time_column = $27, last_change_time_format = $28, incremental = $29, " +
-		"properties_to_unset = $30"
+		"transformation_mapping = $6, transformation_id = $7, transformation_version = $8, transformation_language = $9, " +
+		"transformation_source = $10, transformation_preserve_json = $11, transformation_in_paths = $12, " +
+		"transformation_out_paths = $13, query = $14, format = $15, path = $16, sheet = $17, " +
+		"compression = $18, order_by = $19, format_settings = $20, export_mode = $21, matching_in = $22, " +
+		"matching_out = $23, update_on_duplicates = $24, table_name = $25, table_key = $26, " +
+		"identity_column = $27, last_change_time_column = $28, last_change_time_format = $29, incremental = $30, " +
+		"properties_to_unset = $31"
 	if (c.Role == state.Source && !action.Incremental) || shouldReload(this.action, &n) {
 		update += ", cursor = '0001-01-01 00:00:00+00'"
 	}
-	update += "\nWHERE id = $31"
+	update += "\nWHERE id = $32"
 
 	err = this.core.state.Transaction(ctx, func(tx *state.Tx) error {
 		var function state.TransformationFunction
-		if n.Transformation.Function != nil {
+		if fn := n.Transformation.Function; fn != nil {
 			var current state.TransformationFunction
-			if n.Transformation.Function.Version == "" {
-				err := tx.QueryRow(ctx, "SELECT transformation_source, transformation_language, transformation_version "+
-					"FROM actions WHERE id = $1", n.ID).Scan(&current.Source, &current.Language, &current.Version)
+			if fn.ID == "" {
+				err := tx.QueryRow(ctx, "SELECT transformation_id, transformation_version, transformation_language, transformation_source "+
+					"FROM actions WHERE id = $1", n.ID).Scan(&current.ID, &current.Version, &current.Language, current.Source)
 				if err != nil {
 					return err
 				}
-				if current.Source != n.Transformation.Function.Source || current.Language != n.Transformation.Function.Language {
+				if current.Language != fn.Language || current.Source != fn.Source {
 					return fmt.Errorf("abort update action %d: it was optimistically assumed that the transformation"+
 						" had not changed, but it has indeed changed", n.ID)
 				}
-				n.Transformation.Function.Version = current.Version
+				fn.ID = current.ID
+				fn.Version = current.Version
 			}
-			function = *n.Transformation.Function
+			function = *fn
 		}
 		if c.Role == state.Source && this.action.Target == state.Users {
 			var prevOutPaths []string
@@ -797,7 +790,7 @@ func (this *Action) Update(ctx context.Context, action ActionToSet) error {
 		}
 		result, err := tx.Exec(ctx, update,
 			n.Name, n.Enabled, rawInSchema, rawOutSchema, string(n.Filter), mapping,
-			function.Source, function.Language, function.Version, function.PreserveJSON, n.Transformation.InPaths,
+			function.ID, function.Version, function.Language, function.Source, function.PreserveJSON, n.Transformation.InPaths,
 			n.Transformation.OutPaths, n.Query, formatName, n.Path, n.Sheet, n.Compression, n.OrderBy,
 			string(n.FormatSettings), n.ExportMode, n.Matching.In, n.Matching.Out, n.UpdateOnDuplicates, n.TableName,
 			n.TableKey, n.IdentityColumn, n.LastChangeTimeColumn, n.LastChangeTimeFormat, n.Incremental, n.PropertiesToUnset,
@@ -874,8 +867,8 @@ func (this *Action) fromState(core *Core, store *datastore.Store, action *state.
 	if function := action.Transformation.Function; function != nil {
 		this.Transformation = &Transformation{
 			Function: &TransformationFunction{
-				Source:       function.Source,
 				Language:     Language(function.Language.String()),
+				Source:       function.Source,
 				PreserveJSON: function.PreserveJSON,
 				InPaths:      slices.Clone(action.Transformation.InPaths),
 				OutPaths:     slices.Clone(action.Transformation.OutPaths),
@@ -1318,8 +1311,8 @@ func toStateTransformation(transformation *Transformation, inSchema, outSchema t
 	}
 	return state.Transformation{
 		Function: &state.TransformationFunction{
-			Source:       fn.Source,
 			Language:     language,
+			Source:       fn.Source,
 			PreserveJSON: fn.PreserveJSON,
 		},
 		InPaths:  fn.InPaths,

core/action_validate_test.go
@@ -119,12 +119,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -360,12 +360,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"user"},
 						OutPaths: []string{"email_out"},
 					},
@@ -386,12 +386,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": "a@",`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{},
 						OutPaths: []string{"email_out"},
 					},
@@ -472,12 +472,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -578,12 +578,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(event: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": event["traits"]["email"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"traits"},
 						OutPaths: []string{"email_out"},
 					},
@@ -604,12 +604,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(event: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": "a@b",`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{},
 						OutPaths: []string{"email_out"},
 					},
@@ -659,13 +659,13 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`        "first_name": user["first_name"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in", "first_name"},
 						OutPaths: []string{"email_out", "first_name"},
 					},
@@ -788,12 +788,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths: []string{
 							"email_in",
 							"additional_properties.a",
@@ -940,12 +940,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -966,12 +966,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -1047,12 +1047,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "SomeWeirdLanguage",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "SomeWeirdLanguage",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -1309,12 +1309,12 @@ func Test_validateAction(t *testing.T) {
 						"email_out": "email_in",
 					},
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -2085,12 +2085,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -2139,12 +2139,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -2169,12 +2169,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -2440,13 +2440,13 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`        "in": user["in"]`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in", "id"},
 						OutPaths: []string{"email_out", "id"},
 					},
@@ -2586,12 +2586,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths: []string{
 							"email_in",
 							"additional_properties.a",
@@ -2649,12 +2649,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email"},
 					},
@@ -2806,12 +2806,12 @@ func Test_validateAction(t *testing.T) {
 				}),
 				Transformation: &Transformation{
 					Function: &TransformationFunction{
+						Language: "Python",
 						Source: strings.Join([]string{
 							`def transform(user: dict) -> dict:`,
 							`    return {`,
 							`        "email_out": user["email_in"],`,
 							`    }`}, "\n"),
-						Language: "Python",
 						InPaths:  []string{"email_in"},
 						OutPaths: []string{"email_out"},
 					},
@@ -2939,20 +2939,20 @@ type testProvider struct{}
 
 var _ transformers.Provider = testProvider{}
 
-func (testProvider) Call(ctx context.Context, name, version string, inSchema, outSchema types.Type, preserveJSON bool, records []transformers.Record) error {
+func (testProvider) Call(ctx context.Context, id, version string, inSchema, outSchema types.Type, preserveJSON bool, records []transformers.Record) error {
 	panic("not implemented")
 }
 func (testProvider) Close(ctx context.Context) error { panic("not implemented") }
-func (testProvider) Create(ctx context.Context, name, source string) (string, error) {
+func (testProvider) Create(ctx context.Context, name string, language state.Language, source string) (string, string, error) {
 	panic("not implemented")
 }
-func (testProvider) Delete(ctx context.Context, name string) error {
+func (testProvider) Delete(ctx context.Context, id string) error {
 	panic("not implemented")
 }
 func (testProvider) SupportLanguage(language state.Language) bool {
 	return language == state.JavaScript || language == state.Python
 }
-func (testProvider) Update(ctx context.Context, name, source string) (string, error) {
+func (testProvider) Update(ctx context.Context, id, source string) (string, error) {
 	panic("not implemented")
 }
 

core/connection.go
@@ -817,13 +817,14 @@ func (this *Connection) CreateAction(ctx context.Context, target Target, eventTy
 	}
 
 	var function state.TransformationFunction
-	if n.Transformation.Function != nil {
-		name := util.TransformationFunctionName(n.ID, n.Transformation.Function.Language)
-		version, err := this.core.transformerProvider.Create(ctx, name, n.Transformation.Function.Source)
+	if fn := n.Transformation.Function; fn != nil {
+		name := util.TransformationFunctionName(n.ID)
+		id, version, err := this.core.transformerProvider.Create(ctx, name, fn.Language, fn.Source)
 		if err != nil {
 			return 0, err
 		}
-		n.Transformation.Function.Version = version
+		fn.ID = id
+		fn.Version = version
 		function = *n.Transformation.Function
 	}
 
@@ -869,15 +870,16 @@ func (this *Connection) CreateAction(ctx context.Context, target Target, eventTy
 		}
 		query := "INSERT INTO actions (id, connection, target, event_type, name, enabled,\n" +
 			"schedule_start, schedule_period, in_schema, out_schema, filter, transformation_mapping,\n" +
-			"transformation_source, transformation_language, transformation_version, transformation_preserve_json,\n" +
-			"transformation_in_paths, transformation_out_paths, query, format, path, sheet, compression, order_by,\n" +
-			"format_settings, export_mode, matching_in, matching_out, update_on_duplicates, table_name, table_key,\n" +
-			"identity_column, last_change_time_column, last_change_time_format, incremental)\n" +
+			"transformation_id, transformation_version, transformation_language, transformation_source,\n" +
+			"transformation_preserve_json, transformation_in_paths, transformation_out_paths, query, format, path,\n" +
+			"sheet, compression, order_by, format_settings, export_mode, matching_in, matching_out,\n" +
+			"update_on_duplicates, table_name, table_key, identity_column, last_change_time_column,\n" +
+			"last_change_time_format, incremental)\n" +
 			"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21,\n" +
-			"$22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35)"
+			"$22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)"
 		_, err := tx.Exec(ctx, query, n.ID, n.Connection, n.Target, n.EventType,
 			n.Name, n.Enabled, n.ScheduleStart, n.SchedulePeriod, rawInSchema, rawOutSchema,
-			string(n.Filter), mapping, function.Source, function.Language, function.Version, function.PreserveJSON,
+			string(n.Filter), mapping, function.ID, function.Version, function.Language, function.Source, function.PreserveJSON,
 			n.Transformation.InPaths, n.Transformation.OutPaths, n.Query, formatName, n.Path, n.Sheet,
 			n.Compression, n.OrderBy, string(n.FormatSettings), n.ExportMode, n.Matching.In, n.Matching.Out, n.UpdateOnDuplicates,
 			n.TableName, n.TableKey, n.IdentityColumn, n.LastChangeTimeColumn, n.LastChangeTimeFormat, n.Incremental)
@@ -1493,7 +1495,7 @@ func (this *Connection) PreviewSendEvent(ctx context.Context, typ string, event 
 			action.Transformation.Function.PreserveJSON = transformation.Function.PreserveJSON
 			action.Transformation.InPaths = types.PropertyNames(action.InSchema)
 			action.Transformation.OutPaths = types.PropertyNames(action.OutSchema)
-			provider = newTempTransformerProvider(name, transformation.Function.Source, this.core.transformerProvider)
+			provider = newTempTransformerProvider(name, action.Transformation.Function.Language, action.Transformation.Function.Source, this.core.transformerProvider)
 		default:
 			return nil, errors.BadRequest("transformation mapping or function is required")
 		}
@@ -2291,24 +2293,25 @@ type ConnectionToSet struct {
 // method that is not CallFunction panics.
 type tempTransformerProvider struct {
 	name     string                // function name.
+	language state.Language        // language.
 	source   string                // source code.
 	provider transformers.Provider // underlying transformer provider.
 }
 
-func newTempTransformerProvider(name, source string, provider transformers.Provider) *tempTransformerProvider {
-	return &tempTransformerProvider{name, source, provider}
+func newTempTransformerProvider(name string, language state.Language, source string, provider transformers.Provider) *tempTransformerProvider {
+	return &tempTransformerProvider{name, language, source, provider}
 }
 
 func (tp *tempTransformerProvider) Call(ctx context.Context, _, _ string, inSchema, outSchema types.Type, preserveJSON bool, records []transformers.Record) error {
-	version, err := tp.provider.Create(ctx, tp.name, tp.source)
+	id, version, err := tp.provider.Create(ctx, tp.name, tp.language, tp.source)
 	if err != nil {
 		return nil
 	}
 	defer func() {
 		go func() {
-			err := tp.provider.Delete(context.Background(), tp.name)
+			err := tp.provider.Delete(context.Background(), id)
 			if err != nil {
-				slog.Warn("cannot delete transformation function", "name", tp.name, "err", err)
+				slog.Warn("cannot delete transformation function", "id", id, "err", err)
 			}
 		}()
 	}()
@@ -2316,7 +2319,7 @@ func (tp *tempTransformerProvider) Call(ctx context.Context, _, _ string, inSche
 }
 
 func (tp *tempTransformerProvider) Close(_ context.Context) error { panic("not supported") }
-func (tp *tempTransformerProvider) Create(_ context.Context, _, _ string) (string, error) {
+func (tp *tempTransformerProvider) Create(_ context.Context, _ string, _ state.Language, _ string) (string, string, error) {
 	panic("not supported")
 }
 func (tp *tempTransformerProvider) Delete(_ context.Context, _ string) error {

core/core.go
@@ -673,7 +673,7 @@ func (core *Core) TransformData(ctx context.Context, data []byte, inSchema, outS
 		action.Transformation.Function.PreserveJSON = transformation.Function.PreserveJSON
 		action.Transformation.InPaths = types.PropertyNames(action.InSchema)
 		action.Transformation.OutPaths = types.PropertyNames(action.OutSchema)
-		provider = newTempTransformerProvider(name, transformation.Function.Source, core.transformerProvider)
+		provider = newTempTransformerProvider(name, action.Transformation.Function.Language, action.Transformation.Function.Source, core.transformerProvider)
 	default:
 		return nil, errors.BadRequest("mapping (or transformation) is required")
 	}
@@ -771,19 +771,20 @@ func (core *Core) mustBeOpen() {
 
 // onDeleteAction is called when an action is deleted.
 func (core *Core) onDeleteAction(n state.DeleteAction) {
-	if core.state.IsLeader() && core.transformerProvider != nil {
-		go func() {
-			for _, language := range [...]state.Language{state.JavaScript, state.Python} {
-				if core.transformerProvider.SupportLanguage(language) {
-					name := util.TransformationFunctionName(n.ID, language)
-					err := core.transformerProvider.Delete(core.close.ctx, name)
-					if err != nil {
-						slog.Debug("cannot delete transformer function", "name", name, "err", err)
-					}
-				}
-			}
-		}()
+	if !core.state.IsLeader() || core.transformerProvider == nil {
+		return
 	}
+	action := n.Action()
+	fn := action.Transformation.Function
+	if fn == nil {
+		return
+	}
+	go func() {
+		err := core.transformerProvider.Delete(core.close.ctx, fn.ID)
+		if err != nil {
+			slog.Debug("cannot delete transformer function", "id", fn.ID, "err", err)
+		}
+	}()
 }
 
 // onExecuteAction is called when an action is executed.

core/state/load.go
@@ -367,11 +367,11 @@ func (state *State) load(connectorsOAuth map[string]*ConnectorOAuth) error {
 
 		// Read all actions.
 		err = state.db.QueryScan(ctx, "SELECT id, connection, target, event_type, name, enabled, schedule_start,\n"+
-			"schedule_period, in_schema, out_schema, filter, transformation_mapping, transformation_source,\n"+
-			"transformation_language, transformation_version, transformation_preserve_json, transformation_in_paths,\n"+
-			"transformation_out_paths, query, format, path, sheet, compression::TEXT, order_by, format_settings, export_mode,\n"+
-			"matching_in, matching_out, update_on_duplicates, table_name, table_key, identity_column,\n"+
-			"last_change_time_column, last_change_time_format, health, properties_to_unset\n"+
+			"schedule_period, in_schema, out_schema, filter, transformation_mapping, transformation_id,\n"+
+			"transformation_version, transformation_language, transformation_source, transformation_preserve_json,\n"+
+			"transformation_in_paths, transformation_out_paths, query, format, path, sheet, compression::TEXT,\n"+
+			"order_by, format_settings, export_mode, matching_in, matching_out, update_on_duplicates, table_name,\n"+
+			"table_key, identity_column, last_change_time_column, last_change_time_format, health, properties_to_unset\n"+
 			"FROM actions",
 			func(rows *db.Rows) error {
 				for rows.Next() {
@@ -383,7 +383,7 @@ func (state *State) load(connectorsOAuth map[string]*ConnectorOAuth) error {
 					action := Action{}
 					err := rows.Scan(&action.ID, &connectionID, &action.Target, &eventType, &action.Name,
 						&action.Enabled, &action.ScheduleStart, &action.SchedulePeriod, &rawInSchema, &rawOutSchema,
-						&filter, &mapping, &function.Source, &function.Language, &function.Version, &function.PreserveJSON,
+						&filter, &mapping, &function.ID, &function.Version, &function.Language, &function.Source, &function.PreserveJSON,
 						&action.Transformation.InPaths, &action.Transformation.OutPaths, &action.Query, &format,
 						&action.Path, &action.Sheet, &action.Compression, &action.OrderBy, &action.FormatSettings, &action.ExportMode,
 						&action.Matching.In, &action.Matching.Out, &action.UpdateOnDuplicates, &action.TableName,

core/state/state.go
@@ -1152,9 +1152,10 @@ type Transformation struct {
 
 // TransformationFunction represents a transformation function.
 type TransformationFunction struct {
-	Source       string
-	Language     Language
+	ID           string
 	Version      string
+	Language     Language
+	Source       string
 	PreserveJSON bool
 }
 

commit 307247c7b829b9747d93437acabc02093d9ab132

core: fix action update issue caused by commit 0922e0d
This change fixes issue introduced by commit 0922e0d when updating an
action.

core/action.go
@@ -758,7 +758,7 @@ func (this *Action) Update(ctx context.Context, action ActionToSet) error {
 			var current state.TransformationFunction
 			if fn.ID == "" {
 				err := tx.QueryRow(ctx, "SELECT transformation_id, transformation_version, transformation_language, transformation_source "+
-					"FROM actions WHERE id = $1", n.ID).Scan(&current.ID, &current.Version, &current.Language, current.Source)
+					"FROM actions WHERE id = $1", n.ID).Scan(&current.ID, &current.Version, &current.Language, &current.Source)
 				if err != nil {
 					return err
 				}