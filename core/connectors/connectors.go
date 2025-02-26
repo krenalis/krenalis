a5319866b2273fc31df2addcf1c63709ffcea6ef
Marco Gazerro <gazerro@open2b.com>
2025-02-26 10:33:22 +0100

    core/connectors: fix `SetSettings` for file connectors column rename
    This change fixes the `SetSettings` function passed to file connectors
    in certain cases. When saving the settings to the database, an error
    occurs because the column is no longer called "settings" but
    "format_settings".

--- core/connectors/connectors.go
+++ core/connectors/connectors.go
@@ -615,7 +615,7 @@ func setActionSettings(ctx context.Context, st *state.State, action int, setting
 		Settings: settings,
 	}
 	err := st.Transaction(ctx, func(tx *state.Tx) error {
-		_, err := tx.Exec(ctx, "UPDATE actions SET settings = $1 WHERE id = $2", n.Settings, n.Action)
+		_, err := tx.Exec(ctx, "UPDATE actions SET format_settings = $1 WHERE id = $2", n.Settings, n.Action)
 		if err != nil {
 			return err
 		}
