b9508e828f94d69f5e3aa919678c71ad27432921
Gianluca Mondini <mondini@open2b.com>
2025-03-05 16:23:08 +0100

    test: extend tests on Parquet
    This commit extends the Parquet import test by storing the values of the
    Parquet imported properties into a JSON property of the user schema,
    which is then read by the test and compared to expected values.

--- test/parquet_import_test.go
+++ test/parquet_import_test.go
@@ -8,8 +8,11 @@
 package test
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/meergo/meergo/test/meergotester"
@@ -22,20 +25,34 @@ func TestParquetImport(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-	c := meergotester.InitAndLaunch(t)
+	c := meergotester.InitAndLaunch(t, meergotester.DoNotPopulateUserSchema)
 	defer c.Stop()
 
+	// Change the user schema, leaving only the properties used by this test.
+	userSchemaProperties := []types.Property{}
+	userSchemaProperties = append(userSchemaProperties, types.Property{
+		Name:         "parquet_id",
+		Type:         types.Int(64),
+		ReadOptional: true,
+	})
+	userSchemaProperties = append(userSchemaProperties, types.Property{
+		Name:         "parquet_imported",
+		Type:         types.JSON(),
+		ReadOptional: true,
+	})
+	c.UpdateUserSchema(types.Object(userSchemaProperties), nil, nil)
+
+	// Retrieve the storage directory that contains the Parquet file to import.
 	storageDir, err := filepath.Abs("./testdata/parquet_import_test")
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	if _, err := os.Stat(storageDir); err != nil {
 		t.Fatal(err)
 	}
 
+	// Create a Filesystem source connection, with an action that imports from the Parquet file.
 	fs := c.CreateSourceFilesystem(storageDir)
-
 	action1 := c.CreateAction(fs, "Users", meergotester.ActionToSet{
 		Name:    "Parquet",
 		Enabled: true,
@@ -44,29 +61,81 @@ func TestParquetImport(t *testing.T) {
 			{Name: "parquet_id", Type: types.Int(64), Nullable: true},
 			{Name: "first_name", Type: types.Text(), Nullable: true},
 			{Name: "last_name", Type: types.Text(), Nullable: true},
+			{Name: "date_of_birth", Type: types.Date(), Nullable: true},
+			{Name: "updated_at", Type: types.DateTime(), Nullable: true},
+			{Name: "lunch_time", Type: types.Time(), Nullable: true},
+			{Name: "score", Type: types.Decimal(9, 5), Nullable: true},
 		}),
 		OutSchema: types.Object([]types.Property{
-			{Name: "first_name", Type: types.Text().WithCharLen(300), ReadOptional: true},
-			{Name: "last_name", Type: types.Text().WithCharLen(300), ReadOptional: true},
+			{Name: "parquet_id", Type: types.Int(64), ReadOptional: true},
+			{Name: "parquet_imported", Type: types.JSON(), ReadOptional: true},
 		}),
+		// Define a transformazion function that imports the Parquet row IDs in
+		// "parquet_id" (it is used for sorting the users before comparing them)
+		// and any other property into the JSON property "parquet_imported".
 		Transformation: &meergotester.Transformation{
-			Mapping: map[string]string{
-				"first_name": "first_name",
-				"last_name":  "last_name",
+			Function: &meergotester.TransformationFunction{
+				Language: "Python",
+				Source: strings.Join([]string{
+					`def transform(user: dict) -> dict:`,
+					`	return {"parquet_id": user["parquet_id"], "parquet_imported": user}`,
+				}, "\n"),
+				InPaths:  []string{"parquet_id", "first_name", "last_name", "date_of_birth", "updated_at", "lunch_time", "score"},
+				OutPaths: []string{"parquet_id", "parquet_imported"},
 			},
 		},
 		IdentityColumn: "parquet_id",
 		Format:         "Parquet",
 	})
 
+	// Import and wait.
 	exec1 := c.ExecuteAction(action1)
-
 	c.WaitForExecutionsCompletionAllowFailed(fs, exec1)
 
-	_, _, count := c.Users([]string{"first_name"}, "dummy_id", false, 0, 1000)
-	const expectedCount = 9
-	if count != expectedCount {
-		t.Fatalf("expected %d user(s), got %d", expectedCount, count)
+	// Check that the count of users imported from the file is correct.
+	users, _, count := c.Users([]string{"parquet_imported"}, "parquet_id", false, 0, 1000)
+	if count != len(expectedUsers) {
+		t.Fatalf("expected %d user(s), got %d", len(expectedUsers), count)
 	}
 
+	// Check that the users properties imported in Meergo match the user
+	// properties in the Parquet file.
+	var fail bool
+	for i := range users {
+		gotTraits := users[i].Traits["parquet_imported"].(map[string]any)
+		stripNilProperties(gotTraits)
+		expectedTraits := expectedUsers[i]
+		if !reflect.DeepEqual(gotTraits, expectedTraits) {
+			t.Errorf("users[%d]: expected traits %#v, got %#v", i, expectedTraits, gotTraits)
+			fail = true
+		}
+	}
+	if fail {
+		t.Fatal("users do not match")
+	}
+
+}
+
+var expectedUsers = []map[string]any{
+	{"parquet_id": json.Number("100"), "first_name": "John", "last_name": "Lemon"},
+	{"parquet_id": json.Number("101"), "first_name": "Ringo", "last_name": "Planett"},
+	{"parquet_id": json.Number("102")},
+	{"parquet_id": json.Number("103")},
+	{"parquet_id": json.Number("104"), "date_of_birth": "1980-01-02"},
+	{"parquet_id": json.Number("105"), "date_of_birth": "1935-01-02"},
+	{"parquet_id": json.Number("106"), "updated_at": "2012-01-20 07:20:01"},
+	{"parquet_id": json.Number("107"), "lunch_time": "13:30:00"},
+	{"parquet_id": json.Number("108"), "score": "-1234.56789"},
+}
+
+func stripNilProperties(properties map[string]any) {
+	var toStrip []string
+	for k, v := range properties {
+		if v == nil {
+			toStrip = append(toStrip, k)
+		}
+	}
+	for _, p := range toStrip {
+		delete(properties, p)
+	}
 }
