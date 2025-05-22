e79a53d0948bdc132a3e770fce867f04c681f250
Marco Gazerro <gazerro@open2b.com>
2025-05-22 14:26:20 +0200

    meergo: merge connector and warehouse files
    This change merges `registry_validate_connectors.go` into `registry.go`
    and `warehouses_expr.go` into `warehouses.go`.
    
    This simplifies the repository structure by grouping related logic and
    keeping the root directory cleaner.

--- registry.go
+++ registry.go
@@ -8,9 +8,16 @@
 package meergo
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
 	"reflect"
 	"sync"
+	"time"
+
+	"github.com/meergo/meergo/json"
+	"github.com/meergo/meergo/types"
 )
 
 var (
@@ -347,4 +354,242 @@ func WarehouseDrivers() []WarehouseDriver {
 	}
 	registryMu.Unlock()
 	return drivers
+}
+
+// validateAppConnector validates the passed app connector, performing checks to
+// detect errors that could cause panic or errors in the Meergo code that uses
+// the connectors.
+//
+// In case of a validation error, this function panics.
+func validateAppConnector(app AppInfo) {
+
+	if app.AsSource == nil && app.AsDestination == nil {
+		panic(fmt.Sprintf("connector %s: AppInfo must include at least the AsSource and AsDestination fields", app.Name))
+	}
+
+	if app.AsSource != nil {
+		targets := app.AsSource.Targets
+		if targets == 0 || (targets&^(UsersTarget|GroupsTarget)) != 0 {
+			panic(fmt.Sprintf("connector %s: AppInfo.AsSource.Target is not valid; possible values are meergo.UsersTarget, meergo.GroupsTarget, or a combination of them using the bitwise OR operator", app.Name))
+		}
+		if targets&UsersTarget != 0 {
+			iface := reflect.TypeFor[interface {
+				Records(ctx context.Context, target Targets, lastChangeTime time.Time, ids, properties []string, cursor string, schema types.Type) ([]Record, string, error)
+				Schema(ctx context.Context, target Targets, role Role, eventType string) (types.Type, error)
+			}]()
+			if !app.ct.Implements(iface) {
+				panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+			}
+		}
+		if targets&GroupsTarget != 0 {
+			// TODO(Gianluca): Groups are currently not supported, see
+			// https://github.com/meergo/meergo/issues/895.
+			panic(fmt.Sprintf("connector %s: AppInfo.AsSource.Target includes GroupsTarget, but groups are currently not supported by Meergo (see https://github.com/meergo/meergo/issues/895).", app.Name))
+		}
+	}
+
+	if app.AsDestination != nil {
+		targets := app.AsDestination.Targets
+		if targets == 0 || (targets&^(EventsTarget|UsersTarget|GroupsTarget)) != 0 {
+			panic(fmt.Sprintf("connector %s: AppInfo.AsDestination.Target is not valid; possible values are meergo.EventsTarget, meergo.UsersTarget, meergo.GroupsTarget, or a combination of them using the bitwise OR operator", app.Name))
+		}
+		if targets&UsersTarget != 0 {
+			iface := reflect.TypeFor[interface {
+				Records(ctx context.Context, target Targets, lastChangeTime time.Time, ids, properties []string, cursor string, schema types.Type) ([]Record, string, error)
+				Schema(ctx context.Context, target Targets, role Role, eventType string) (types.Type, error)
+				Upsert(ctx context.Context, target Targets, records Records) error
+			}]()
+			if !app.ct.Implements(iface) {
+				panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+			}
+		}
+		if targets&GroupsTarget != 0 {
+			// TODO(Gianluca): Groups are currently not supported, see
+			// https://github.com/meergo/meergo/issues/895.
+			panic(fmt.Sprintf("connector %s: AppInfo.AsDestination.Target includes GroupsTarget, but groups are currently not supported by Meergo (see https://github.com/meergo/meergo/issues/895).", app.Name))
+		}
+		if targets&EventsTarget != 0 {
+			iface := reflect.TypeFor[interface {
+				EventRequest(ctx context.Context, event RawEvent, eventType string, schema types.Type, properties map[string]any, redacted bool) (*EventRequest, error)
+				EventTypes(ctx context.Context) ([]*EventType, error)
+				Schema(ctx context.Context, target Targets, role Role, eventType string) (types.Type, error)
+			}]()
+			if !app.ct.Implements(iface) {
+				panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+			}
+			if app.AsDestination.SendingMode == None {
+				panic(fmt.Sprintf("connector %s is declared to support Events as destination, but it does not specify a sending mode", app.Name))
+			}
+		}
+	}
+
+	if app.Terms.User != "" || app.Terms.Users != "" {
+		if (app.AsSource == nil || app.AsSource.Targets&UsersTarget == 0) &&
+			(app.AsDestination == nil || app.AsDestination.Targets&UsersTarget == 0) {
+			panic(fmt.Sprintf("connector %s: cannot specify a term for user and/or users"+
+				" if it does not support the Users target neither as source nor as destination", app.Name))
+		}
+	}
+
+	if app.Terms.Group != "" || app.Terms.Groups != "" {
+		if (app.AsSource == nil || app.AsSource.Targets&GroupsTarget == 0) &&
+			(app.AsDestination == nil || app.AsDestination.Targets&GroupsTarget == 0) {
+			panic(fmt.Sprintf("connector %s: cannot specify a term for group and/or groups"+
+				" if it does not support the Groups target neither as source nor as destination", app.Name))
+		}
+	}
+
+	var hasSourceSettings = app.AsSource != nil && app.AsSource.HasSettings
+	var hasDestinationSettings = app.AsDestination != nil && app.AsDestination.HasSettings
+	if hasSourceSettings || hasDestinationSettings {
+		iface := reflect.TypeFor[interface {
+			ServeUI(ctx context.Context, event string, settings json.Value, role Role) (*UI, error)
+		}]()
+		if !app.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+		}
+	} else if !hasSourceSettings && !hasDestinationSettings {
+		iface := reflect.TypeFor[interface {
+			ServeUI(ctx context.Context, event string, settings json.Value, role Role) (*UI, error)
+		}]()
+		if app.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: ServeUI is implemented, but neither app.AsSource.HasSettings nor app.AsDestination.HasSettings is set to true", app.Name))
+		}
+	}
+
+	if app.OAuth.AuthURL != "" {
+		iface := reflect.TypeFor[interface {
+			OAuthAccount(ctx context.Context) (string, error)
+		}]()
+		if !app.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+		}
+	}
+
+	if app.WebhooksPer != WebhooksPerNone {
+		iface := reflect.TypeFor[interface {
+			ReceiveWebhook(r *http.Request, role Role) ([]WebhookPayload, error)
+		}]()
+		if !app.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", app.Name))
+		}
+	}
+
+}
+
+// validateDatabaseConnector validates the passed database connector, performing
+// checks to detect errors that could cause panic or errors in the Meergo code
+// that uses the connectors.
+//
+// In case of a validation error, this function panics.
+func validateDatabaseConnector(database DatabaseInfo) {
+	iface := reflect.TypeFor[interface {
+		Close() error
+		Columns(ctx context.Context, table string) ([]Column, error)
+		Merge(ctx context.Context, table Table, rows [][]any) error
+		Query(ctx context.Context, query string) (Rows, []Column, error)
+		QuoteTime(value any, typ types.Type) string
+		ServeUI(ctx context.Context, event string, settings json.Value, role Role) (*UI, error)
+	}]()
+	if !database.ct.Implements(iface) {
+		panic(fmt.Sprintf("connector %s: it does not implement the required methods", database.Name))
+	}
+}
+
+// validateFileConnector validates the passed file connector, performing checks
+// to detect errors that could cause panic or errors in the Meergo code that
+// uses the connectors.
+//
+// In case of a validation error, this function panics.
+func validateFileConnector(file FileInfo) {
+
+	if file.AsSource != nil {
+		iface := reflect.TypeFor[interface {
+			Read(ctx context.Context, r io.Reader, sheet string, records RecordWriter) error
+		}]()
+		if !file.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: inconsistency between the declared functionalities and the methods it actually implements", file.Name))
+		}
+	}
+
+	if file.AsDestination != nil {
+		iface := reflect.TypeFor[interface {
+			Write(ctx context.Context, w io.Writer, sheet string, records RecordReader) error
+			ContentType(ctx context.Context) string
+		}]()
+		if !file.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", file.Name))
+		}
+	}
+
+	if file.HasSheets {
+		iface := reflect.TypeFor[interface {
+			Sheets(ctx context.Context, r io.Reader) ([]string, error)
+		}]()
+		if !file.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", file.Name))
+		}
+	}
+
+	if (file.AsSource != nil && file.AsSource.HasSettings) ||
+		(file.AsDestination != nil && file.AsDestination.HasSettings) {
+		iface := reflect.TypeFor[interface {
+			ServeUI(ctx context.Context, event string, settings json.Value, role Role) (*UI, error)
+		}]()
+		if !file.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", file.Name))
+		}
+	}
+
+}
+
+// validateFileStorageConnector validates the passed file storage connector,
+// performing checks to detect errors that could cause panic or errors in the
+// Meergo code that uses the connectors.
+//
+// In case of a validation error, this function panics.
+func validateFileStorageConnector(fileStorage FileStorageInfo) {
+
+	iface := reflect.TypeFor[interface {
+		AbsolutePath(ctx context.Context, name string) (string, error)
+		ServeUI(ctx context.Context, event string, settings json.Value, role Role) (*UI, error)
+	}]()
+	if !fileStorage.ct.Implements(iface) {
+		panic(fmt.Sprintf("connector %s: it does not implement the minimum required methods", fileStorage.Name))
+	}
+
+	if fileStorage.AsSource {
+		iface := reflect.TypeFor[interface {
+			Reader(ctx context.Context, name string) (io.ReadCloser, time.Time, error)
+		}]()
+		if !fileStorage.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", fileStorage.Name))
+		}
+	}
+
+	if fileStorage.AsDestination {
+		iface := reflect.TypeFor[interface {
+			Write(ctx context.Context, r io.Reader, name, contentType string) error
+		}]()
+		if !fileStorage.ct.Implements(iface) {
+			panic(fmt.Sprintf("connector %s: there's a mismatch between the declared functionalities and the methods actually implemented", fileStorage.Name))
+		}
+	}
+
+}
+
+// validateStreamConnector validates the passed stream connector, performing
+// checks to detect errors that could cause panic or errors in the Meergo code
+// that uses the connectors.
+//
+// In case of a validation error, this function panics.
+func validateStreamConnector(stream StreamInfo) {
+	iface := reflect.TypeFor[interface {
+		Close() error
+		Receive(ctx context.Context) (event []byte, ack func(), err error)
+		Send(ctx context.Context, event []byte, options SendOptions, ack func(err error)) error
+	}]()
+	if !stream.ct.Implements(iface) {
+		panic(fmt.Sprintf("connector %s: it does not implement the required methods", stream.Name))
+	}
 }
--- warehouses.go
+++ warehouses.go
@@ -557,3 +557,77 @@ func (e *WarehouseSettingsError) Error() string {
 func WarehouseSettingsErrorf(format string, a ...any) error {
 	return &WarehouseSettingsError{Err: fmt.Errorf(format, a...)}
 }
+
+// Expr represents a subset of SQL expressions.
+type Expr interface {
+	expr()
+}
+
+// MultiExpr represents an SQL expression with a logical operator, which can be
+// both And or Or, and a list of SQL expressions on which the operator is
+// applied.
+type MultiExpr struct {
+	Operator LogicalOperator
+	Operands []Expr
+}
+
+func (*MultiExpr) expr() {}
+
+// NewMultiExpr returns a new MultiExpr expression with the given operator and
+// operands.
+func NewMultiExpr(operator LogicalOperator, operands []Expr) *MultiExpr {
+	return &MultiExpr{Operator: operator, Operands: operands}
+}
+
+// LogicalOperator represents the logical operator of a MultiExpr.
+type LogicalOperator int
+
+const (
+	OpAnd LogicalOperator = iota
+	OpOr
+)
+
+// BaseExpr represents an SQL expression that refers to a property, on which an
+// operator is applied, an eventually an operand, if the operator is binary.
+type BaseExpr struct {
+	Column   Column
+	Operator Operator
+	Values   []any // may be nil for unary expressions.
+}
+
+func (*BaseExpr) expr() {}
+
+// NewBaseExpr returns a new BaseExpr expression that applies to the given
+// column with the given operator and values.
+// If the operator is unary, value should be nil.
+func NewBaseExpr(column Column, operator Operator, values ...any) *BaseExpr {
+	return &BaseExpr{Column: column, Operator: operator, Values: values}
+}
+
+// Operator presents a unary or binary operator of a BaseExpr.
+type Operator int
+
+const (
+	OpIs                     Operator = iota // is
+	OpIsNot                                  // is not
+	OpIsLessThan                             // is less than
+	OpIsLessThanOrEqualTo                    // is less than or equal to
+	OpIsGreaterThan                          // is greater than
+	OpIsGreaterThanOrEqualTo                 // is greater than or equal to
+	OpIsBetween                              // is between
+	OpIsNotBetween                           // is not between
+	OpContains                               // contains
+	OpDoesNotContain                         // does not contain
+	OpIsOneOf                                // is one of
+	OpIsNotOneOf                             // is not one of
+	OpStartsWith                             // starts with
+	OpEndsWith                               // ends with
+	OpIsBefore                               // is before
+	OpIsOnOrBefore                           // is on or before
+	OpIsAfter                                // is after
+	OpIsOnOrAfter                            // is on or after
+	OpIsTrue                                 // is true
+	OpIsFalse                                // is false
+	OpIsNull                                 // is null
+	OpIsNotNull                              // is not null
+)
