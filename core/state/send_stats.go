4b7afbe9ce33e5cda4408445d10de2d36999d524
Gianluca Mondini <mondini@open2b.com>
2025-07-03 11:47:49 +0200

    core/state: send the OS to the remote Meergo instance that tracks stats

--- core/state/send_stats.go
+++ core/state/send_stats.go
@@ -9,6 +9,7 @@ package state
 
 import (
 	"log/slog"
+	"runtime"
 
 	"github.com/meergo/meergo/analytics-go"
 )
@@ -22,12 +23,35 @@ func (state *State) sendNotificationStats(client analytics.Client, n notificatio
 	}
 	go func() {
 		err := client.Enqueue(analytics.Track{
-			UserId:     state.metadata.installationID,
-			Event:      "State Changed",
+			UserId: state.metadata.installationID,
+			Event:  "State Changed",
+			Context: &analytics.Context{
+				OS: analytics.OSInfo{
+					Name: eventContextOs,
+				},
+			},
 			Properties: analytics.NewProperties().Set("notification_name", n.Name),
 		})
 		if err != nil {
 			slog.Error("cannot enqueue Track event when sending stats to Meergo", "err", err)
 		}
 	}()
+}
+
+// eventContextOs is the name of the OS on which this Meergo instance is
+// running, in a format accepted by the 'context.os.name' field of the Meergo
+// API endpoint that ingests events.
+var eventContextOs string
+
+func init() {
+	switch runtime.GOOS {
+	case "darwin":
+		eventContextOs = "macOS"
+	case "linux":
+		eventContextOs = "Linux"
+	case "windows":
+		eventContextOs = "Windows"
+	default:
+		eventContextOs = "Other"
+	}
 }
