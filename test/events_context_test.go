3d1283a0bb6f3b4e95e61c5ca902da19fa9df553
Andrea Retini <retini@open2b.com>
2025-07-28 10:49:32 +0200

    test: rename `meergoAPIKey` to `meergoEventWriteKey`

--- test/events_context_test.go
+++ test/events_context_test.go
@@ -28,14 +28,14 @@ func TestEventsContext(t *testing.T) {
 
 	// Create a Meergo API connection, with an action to ingest the events.
 	var meergoAPIID int
-	var meergoAPIKey string
+	var meergoEventWriteKey string
 	{
 		meergoAPIID = c.CreateMeergoAPISource("Meergo API", nil)
 		keys := c.EventWriteKeys(meergoAPIID)
 		if len(keys) != 1 {
 			t.Fatalf("expected one key, got %d keys", len(keys))
 		}
-		meergoAPIKey = keys[0]
+		meergoEventWriteKey = keys[0]
 		c.CreateAction(meergoAPIID, "Event", meergotester.ActionToSet{
 			Name:    "Ingest events",
 			Enabled: true,
@@ -43,28 +43,28 @@ func TestEventsContext(t *testing.T) {
 	}
 
 	// Send various events, with various user agent and OS configurations.
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 1",
 		Context: &analytics.Context{
 			UserAgent: "N/A",
 		},
 	})
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 2",
 		Context: &analytics.Context{
 			UserAgent: "",
 		},
 	})
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 3",
 		Context: &analytics.Context{
 			UserAgent: "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
 		},
 	})
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 4",
 		Context: &analytics.Context{
@@ -75,7 +75,7 @@ func TestEventsContext(t *testing.T) {
 			},
 		},
 	})
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 5",
 		Context: &analytics.Context{
@@ -87,7 +87,7 @@ func TestEventsContext(t *testing.T) {
 			},
 		},
 	})
-	c.SendEvent(meergoAPIKey, analytics.Track{
+	c.SendEvent(meergoEventWriteKey, analytics.Track{
 		AnonymousId: "ff8dee31-fd87-45bb-978b-c7b3e2c52128",
 		Event:       "Test Event 6",
 		Context: &analytics.Context{
