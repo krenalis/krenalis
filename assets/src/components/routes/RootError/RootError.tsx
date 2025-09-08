ca79705a35a49cf3c22a0608fbfa22779bedd6e0
Andrea Retini <retini@open2b.com>
2025-09-08 11:26:02 +0200

    assets: improve the error UI
    This commit improves the error UI by refining the text and providing a button
    that restarts the application with a clean state, to try overcome the error and
    prevent the user from being stuck.
    
    Closes #1781.

--- assets/src/components/routes/RootError/RootError.tsx
+++ assets/src/components/routes/RootError/RootError.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './RootError.css';
 import IconWrapper from '../../base/IconWrapper/IconWrapper';
+import SlButton from '@shoelace-style/shoelace/dist/react/button/index.js';
+import { UI_BASE_PATH } from '../../../constants/paths';
+import { storageKeysToBeRemoved } from '../../../constants/storage';
 
 const RootError = () => {
+	const onRestartApp = () => {
+		sessionStorage.clear();
+		for (const key of storageKeysToBeRemoved) {
+			localStorage.removeItem(key);
+		}
+		window.location.href = `${UI_BASE_PATH}`;
+	};
+
 	return (
 		<div className='root-error'>
 			<div className='root-error__message'>
-				<IconWrapper name='bug' size={40} />
-				<div className='root-error__title'>An error occured in the application</div>
-				<div className='root-error__instructions'>Please contact the administrator</div>
+				<IconWrapper name='exclamation-circle' size={40} />
+				<div className='root-error__title'>An error occurred in the Admin console</div>
+				<div className='root-error__instructions'>If the issue persists, please contact the administrator</div>
+				<SlButton variant='primary' className='root-error__action' onClick={onRestartApp}>
+					Restart the Admin console
+				</SlButton>
 			</div>
 		</div>
 	);
--- assets/src/constants/storage.ts
+++ assets/src/constants/storage.ts
@@ -8,6 +8,20 @@ const USERS_TAB_KEY = 'meergo_ui_users_tab';
 const USERS_EXPANDED_TRAITS_KEY = 'meergo_ui_users_expanded_traits';
 const USERS_PROPERTIES_KEY = 'meergo_ui_users_properties';
 
+// storageKeysToBeRemoved contains the list of keys in browser localStorage that
+// can be cleared when resetting the client state, for example to attempt fixing
+// an unhandled error in the UI.
+const storageKeysToBeRemoved = [
+	ADD_CONNECTOR_NAME_KEY,
+	ADD_CONNECTION_ROLE_KEY,
+	ADD_CONNECTION_ID_KEY,
+	IS_PASSWORDLESS_KEY,
+	IS_DOCKER_KEY,
+	USERS_TAB_KEY,
+	USERS_EXPANDED_TRAITS_KEY,
+	USERS_PROPERTIES_KEY,
+];
+
 export {
 	WORKSPACE_ID_KEY,
 	ADD_CONNECTOR_NAME_KEY,
@@ -18,4 +32,5 @@ export {
 	USERS_TAB_KEY,
 	USERS_EXPANDED_TRAITS_KEY,
 	USERS_PROPERTIES_KEY,
+	storageKeysToBeRemoved,
 };
