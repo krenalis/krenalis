krenalis/krenalis

commit 75a9e88c1d24111611f63ee1d87bcbb876697c58

assets: show integration instructions into all SDK connections
This commit adds in the main page of all SDK connections, a new section
with instructions and a link to documentation for integrating the SDK
inside an application or website.

Close #1607.

assets/src/components/base/Snippet/Snippet.tsx
@@ -2,20 +2,41 @@ import React, { useContext, useEffect, useMemo, useState } from 'react';
 import './Snippet.css';
 import AppContext from '../../../context/AppContext';
 import { NotFoundError } from '../../../lib/api/errors';
-import { SNIPPET } from '../../../constants/javascriptSnippet';
 import SlCopyButton from '@shoelace-style/shoelace/dist/react/copy-button/index.js';
+
 import SyntaxHighlight from '../SyntaxHighlight/SyntaxHighlight';
+import Section from '../../base/Section/Section';
 
 interface SnippetProps {
+	connectorName: string;
 	connectionID: number;
 }
 
-const Snippet = ({ connectionID }: SnippetProps) => {
+interface SnippetFile {
+	SNIPPET: string;
+	INSTALL_COMMAND?: string;
+	DOCUMENTATION_LINK: string;
+}
+
+const Snippet = ({ connectorName, connectionID }: SnippetProps) => {
 	const [keys, setKeys] = useState<string[]>([]);
 	const [eventURL, setEventURL] = useState<string>();
 	const [javaScriptSDKURL, setJavaScriptSDKURL] = useState<string>();
+	const [snippet, setSnippet] = useState<string>();
+	const [installCommand, setInstallCommand] = useState<string>();
+	const [documentationLink, setDocumentationLink] = useState<string>();
 
 	const { api, handleError, redirect } = useContext(AppContext);
+
+	useEffect(() => {
+		import(`../../../constants/snippets/${connectorName.toLowerCase().replace(/\./g, '')}.ts`).then(
+			(file: SnippetFile) => {
+				setSnippet(file.SNIPPET);
+				setInstallCommand(file.INSTALL_COMMAND);
+				setDocumentationLink(file.DOCUMENTATION_LINK);
+			},
+		);
+	}, [connectorName]);
 
 	// Retrieve the event URL.
 	useEffect(() => {
@@ -73,18 +94,76 @@ const Snippet = ({ connectionID }: SnippetProps) => {
 		fetchKeys();
 	}, [connectionID]);
 
-	const snippet = useMemo<string>(() => {
-		const r1 = SNIPPET.replace('"writekey"', JSON.stringify(keys[0]));
-		const r2 = r1.replace('"endpoint"', JSON.stringify(eventURL));
-		const r3 = r2.replace('"javaScriptSDKURL"', JSON.stringify(javaScriptSDKURL));
-		return r3;
-	}, [SNIPPET, keys, eventURL]);
+	const completeSnippet = useMemo<string>(() => {
+		if (snippet == null) {
+			return '';
+		}
+		const s1 = snippet.replace('"writekey"', JSON.stringify(keys[0]));
+		const s2 = s1.replace('"endpoint"', JSON.stringify(eventURL));
+		let s3 = s2;
+		if (connectorName === 'Javascript') {
+			s3 = s2.replace('"javaScriptSDKURL"', JSON.stringify(javaScriptSDKURL));
+		}
+		return s3;
+	}, [connectorName, snippet, keys, eventURL]);
+
+	let applicationType = 'server';
+	if (connectorName === 'Android' || connectorName === 'Apple') {
+		applicationType = 'app';
+	} else if (connectorName === 'JavaScript') {
+		applicationType = 'website';
+	}
+
+	let language = 'html';
+	if (connectorName === 'Python') {
+		language = 'python';
+	} else if (connectorName === 'Go') {
+		language = 'go';
+	} else if (connectorName === '.NET') {
+		language = 'csharp';
+	} else if (connectorName === 'Android') {
+		language = 'kotlin';
+	} else if (connectorName === 'Java') {
+		language = 'java';
+	} else if (connectorName === 'Node.js') {
+		language = 'javascript';
+	}
 
 	return (
-		<div className='snippet'>
-			<SyntaxHighlight language='html'>{snippet}</SyntaxHighlight>
-			<SlCopyButton value={snippet} />
-		</div>
+		<Section
+			title={`Add Meergo to your ${applicationType}`}
+			className='connection-actions__instructions'
+			description={
+				<div className='connection-actions__instructions-text'>
+					Copy this snippet and paste it into your {applicationType} to receive events
+					<a target='_blank' href={documentationLink}>
+						See documentation
+					</a>
+				</div>
+			}
+			annotated={true}
+		>
+			<div className={`snippet${installCommand != null ? ' snippet--command' : ''}`}>
+				{installCommand != null && (
+					<>
+						<SyntaxHighlight
+							className='syntax-highlight--install-command'
+							language={connectorName === 'Java' || connectorName === 'Android' ? 'markdown' : 'bash'}
+							icon={connectorName === 'Java' || connectorName === 'Android' ? 'info-circle' : 'terminal'}
+						>
+							{installCommand}
+						</SyntaxHighlight>
+						{connectorName !== 'Java' && connectorName !== 'Android' && (
+							<SlCopyButton value={installCommand} />
+						)}
+					</>
+				)}
+				<SyntaxHighlight className='syntax-highlight--snippet' language={language}>
+					{completeSnippet}
+				</SyntaxHighlight>
+				<SlCopyButton value={completeSnippet} />
+			</div>
+		</Section>
 	);
 };
 

assets/minify-snippet.js
@@ -11,9 +11,9 @@ esbuild
 	})
 	.then((result) => {
 		const text = result.outputFiles[0].text;
-		let transformed = text.replace(/meergo\.load\([^)]*\)/, '\n\t$&;\n\t');
-		const snippet = `export const SNIPPET = \`<script>\n\t${transformed}</script>\`;\n`;
-		fs.writeFileSync('src/constants/javascriptSnippet.ts', snippet);
+		let transformed = text.replace(/meergo\.load\([^)]*\)/, '\n  $&;\n  ');
+		const snippet = `export const SNIPPET = \`<script>\n  ${transformed}</script>\`;\n\nexport const DOCUMENTATION_LINK = 'http://localhost:8080/developers/javascript-sdk';`;
+		fs.writeFileSync('src/constants/snippets/javascript.ts', snippet);
 	})
 	.catch((err) => {
 		console.error(err);

assets/src/components/base/Snippet/Snippet.css
@@ -1,18 +1,16 @@
 .snippet {
 	position: relative;
 	margin-top: 0;
-	padding: 20px 20px 0 0;
-	box-shadow: var(--sl-shadow-small);
-	border-radius: 10px;
-	background-color: #f4f2fd !important;
 }
 
-.snippet .syntax-highlight pre {
-	padding-bottom: 17px !important;
-	padding-left: 20px !important;
+.syntax-highlight--install-command + sl-copy-button {
+	position: absolute;
+	width: max-content !important;
+	top: 10px;
+	right: 20px;
 }
 
-.snippet sl-copy-button {
+.syntax-highlight--snippet + sl-copy-button {
 	margin-top: 20px;
 	position: absolute;
 	width: max-content !important;
@@ -22,16 +20,11 @@
 
 .snippet sl-copy-button::part(button) {
 	gap: 8px;
-	background-color: var(--color-primary-600);
-	color: var(--text-onaccent);
+	background-color: transparent;
 	padding: 9px 13px;
 	margin-left: auto;
 	font-size: 12px;
-	font-weight: 600;
-}
-
-.snippet sl-copy-button::part(button)::after {
-	content: 'Copy';
+	border: 1px solid var(--sl-color-neutral-300);
 }
 
 .snippet .syntax-highlight pre::-webkit-scrollbar {
@@ -55,4 +48,37 @@
 
 .snippet .syntax-highlight pre::-webkit-scrollbar-thumb:hover {
 	background: #555;
+}
+
+.snippet .syntax-highlight {
+	padding: 15px 20px !important;
+	padding-right: 80px !important;
+}
+
+.snippet .syntax-highlight--snippet {
+	padding-bottom: 0 !important;
+}
+
+.snippet .syntax-highlight--snippet > pre {
+	padding-bottom: 20px !important;
+}
+
+.snippet .syntax-highlight--install-command {
+	background-color: var(--sl-color-neutral-50);
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
+	border: 1px solid var(--sl-color-primary-200);
+	border-bottom: 1px solid var(--sl-color-primary-200);
+}
+
+.snippet .syntax-highlight--snippet {
+	border-radius: 10px;
+	background-color: #f4f2fd !important;
+	border: 1px solid var(--sl-color-primary-200);
+}
+
+.snippet--command .syntax-highlight--snippet {
+	border-top-left-radius: 0;
+	border-top-right-radius: 0;
+	border-top: 0;
 }

assets/src/components/base/SyntaxHighlight/SyntaxHighlight.css
@@ -1,3 +1,15 @@
+.syntax-highlight--icon {
+	display: flex;
+	gap: 7px;
+}
+
+.syntax-highlight--icon sl-icon {
+	position: relative;
+	top: 1.5px;
+	font-size: 15px !important;
+	min-width: 16px !important;
+}
+
 .syntax-highlight pre {
 	background-color: transparent !important;
 	padding: 0 !important;

assets/src/components/base/SyntaxHighlight/SyntaxHighlight.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import './SyntaxHighlight.css';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { arduinoLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import SlIcon from '@shoelace-style/shoelace/dist/react/icon/index.js';
 
 interface SyntaxHighlightProps {
 	children: ReactNode;
@@ -9,6 +10,8 @@ interface SyntaxHighlightProps {
 	showLineNumbers?: boolean;
 	wrapLines?: boolean;
 	lineNumberStyle?: any;
+	icon?: string;
+	className?: string;
 	lineProps?: (lineNumber: number) => any;
 }
 
@@ -18,10 +21,15 @@ const SyntaxHighlight = ({
 	showLineNumbers,
 	wrapLines = false,
 	lineNumberStyle,
+	icon,
+	className,
 	lineProps,
 }: SyntaxHighlightProps) => {
 	return (
-		<div className='syntax-highlight'>
+		<div
+			className={`syntax-highlight${className != null ? ' ' + className : ''}${icon != null ? ' syntax-highlight--icon' : ''}`}
+		>
+			{icon != null && <SlIcon name={icon} />}
 			<SyntaxHighlighter
 				language={language ? language : 'javascript'}
 				showLineNumbers={showLineNumbers}

assets/src/components/routes/ConnectionActions/ConnectionActions.tsx
@@ -113,21 +113,7 @@ const ConnectionActions = () => {
 			className={`connection-actions${connection.actions!.length === 0 ? ' connection-actions--no-action' : ''}`}
 		>
 			{connection.connector.hasSnippet && (
-				<Section
-					title='Add Meergo to your website'
-					className='connection-actions__instructions'
-					description={
-						<div className='connection-actions__instructions-text'>
-							Copy this snippet and paste it into your website to receive events
-							<a target='_blank' href='http://localhost:8080/developers/javascript-sdk'>
-								See documentation
-							</a>
-						</div>
-					}
-					annotated={true}
-				>
-					<Snippet connectionID={connection.id} />
-				</Section>
+				<Snippet connectorName={connection.connector.name} connectionID={connection.id} />
 			)}
 			{/* Linked connections are shown: before the actions, in the case of destination actions; after the actions,
 			in the case of source actions. This is to better suggest the usability flow. */}

assets/src/components/routes/ConnectionSettings/ConnectionSettings.css
@@ -130,9 +130,13 @@
 	width: 100%;
 }
 
+.connection-settings__snippet-copy .section__heading {
+	display: none;
+}
+
 .connection-settings__snippet-copy > div:first-child {
 	margin-left: 3px;
-	margin-bottom: 8px;
+	margin-bottom: 10px;
 }
 
 .connection-settings__snippet-copy .snippet {

assets/src/components/routes/ConnectionSettings/ConnectionSettings.tsx
@@ -3,7 +3,6 @@ import './ConnectionSettings.css';
 import ConnectionGeneralSettings from './ConnectionGeneralSettings';
 import ConnectionConnectorSettings from './ConnectionConnectorSettings';
 import ConnectionKeys from './ConnectionKeys';
-import ConnectionSnippet from './ConnectionSnippet';
 import AppContext from '../../../context/AppContext';
 import ConnectionContext from '../../../context/ConnectionContext';
 import SlTab from '@shoelace-style/shoelace/dist/react/tab/index.js';
@@ -13,6 +12,7 @@ import SlTabPanel from '@shoelace-style/shoelace/dist/react/tab-panel/index.js';
 import { ConnectorUIResponse } from '../../../lib/api/types/responses';
 import { debounce } from '../../../utils/debounce';
 import { isSourceEventConnection } from '../../../lib/core/connection';
+import { Snippet } from '../../base/Snippet/Snippet';
 
 type TabName = 'general' | 'snippet' | 'connection' | 'keys';
 
@@ -85,7 +85,7 @@ const ConnectionSettings = () => {
 	const tabs = useMemo(() => {
 		const tabs: TabName[] = ['general'];
 
-		if (c.connector.hasSnippet) {
+		if (c.connector.name === 'JavaScript') {
 			tabs.push('snippet');
 		}
 
@@ -132,7 +132,10 @@ const ConnectionSettings = () => {
 						</SlTab>
 						<SlTabPanel name='snippet'>
 							<div className='connection-settings__panel-title'>Snippet</div>
-							<ConnectionSnippet />
+							<div className='connection-settings__snippet-copy'>
+								<div>Embed the snippet in your website to start sending events:</div>
+								<Snippet connectorName={c.connector.name} connectionID={c.id} />
+							</div>
 						</SlTabPanel>
 					</>
 				)}

commit 15be99b4f4ab2a3eac409f77d3ead8b2b829f8cf

core: correct JavaScript snippet in Admin console
The JavaScript complete snippet in the Admin console used the wrong
connector name. Commit 75a9e88 mistakenly wrote "Javascript" instead of
"JavaScript". This change corrects the naming.

Fixes #1815

assets/src/components/base/Snippet/Snippet.tsx
@@ -101,7 +101,7 @@ const Snippet = ({ connectorName, connectionID }: SnippetProps) => {
 		const s1 = snippet.replace('"writekey"', JSON.stringify(keys[0]));
 		const s2 = s1.replace('"endpoint"', JSON.stringify(externalEventURL));
 		let s3 = s2;
-		if (connectorName === 'Javascript') {
+		if (connectorName === 'JavaScript') {
 			s3 = s2.replace('"javaScriptSDKURL"', JSON.stringify(javaScriptSDKURL));
 		}
 		return s3;