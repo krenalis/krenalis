4d5c212429745c76716c5a7c36d5edfe6df3536d
Gianluca Mondini <mondini@open2b.com>
2025-10-27 15:02:19 +0100

    warehouses/postgresql: update `updates.sql`

--- warehouses/postgresql/updates.sql
+++ warehouses/postgresql/updates.sql
@@ -1,3 +1,9 @@
 ALTER TABLE "events" RENAME COLUMN "user" TO "muid";
 
 ALTER TABLE "_user_identities" RENAME COLUMN "__gid__" TO "__muid__";
+
+-- Note: replace "_users_0" with the name of the actual users table you have in
+-- your data warehouse.
+ALTER TABLE "_users_0" RENAME COLUMN "__id__" to "__muid__";
+
+ALTER VIEW "users" RENAME COLUMN "__id__" to "__muid__";
