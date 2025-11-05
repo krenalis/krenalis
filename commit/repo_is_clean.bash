66ebcae5086bf99ddff49e8d0153f1c8d2992c57
Gianluca Mondini <mondini@open2b.com>
2025-11-05 14:43:13 +0100

    .github/workflows, commit: implement check for repo and remove dep.
    This commit removes the dependency on the third-party Action
    'numtide/clean-git-action', which ensures that the repository is clean
    after testing, allowing a script within the repository to perform the
    necessary operations.
    
    Resolves #1976.

---
+++ commit/repo_is_clean.bash
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+# Copyright 2025 Open2b. All rights reserved.
+# Use of this source code is governed by an Elastic License 2.0
+# that can be found in the LICENSE file.
+
+# This script is used by the GitHub Action which runs the tests and reformats
+# the repository, ensuring that the checked commit was already formatted
+# correctly.
+
+set -e -o pipefail
+
+if [[ "$(git status --porcelain)" ]]; then
+    echo "The repository is not clean"
+    git status
+    exit 1
+fi
+
+echo "The repository is clean"
