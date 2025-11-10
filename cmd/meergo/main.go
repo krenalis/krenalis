4307d0843adcebf63ff254b84d9d7f82b95e77fe
Marco Gazerro <gazerro@open2b.com>
2025-11-10 09:33:11 +0100

    Improve comment

--- cmd/meergo/main.go
+++ cmd/meergo/main.go
@@ -11,7 +11,7 @@
 //  1. Create a new directory: mkdir meergo
 //  2. Change into it: cd meergo
 //  3. Copy this file into the new directory
-//  4. (optional) Add a new file with your connectors and warehouse drivers:
+//  4. (optional) Add a new file for your connectors and warehouse drivers:
 //     package meergo
 //     import _ "github.com/example/connector"
 //     import _ "github.com/example/warehouse"
