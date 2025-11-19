368c1d561ca655ed332df77b36291a389cef246d
Marco Gazerro <gazerro@open2b.com>
2025-11-19 19:42:44 +0100

    admin: use `meergo_admin_` as storage prefix instead of `meergo_ui_`

--- admin/src/constants/storage.ts
+++ admin/src/constants/storage.ts
@@ -1,12 +1,12 @@
-const WORKSPACE_ID_KEY = 'meergo_ui_workspace_id';
-const ADD_CONNECTOR_CODE_KEY = 'meergo_ui_add_connector_code';
-const ADD_CONNECTION_ROLE_KEY = 'meergo_ui_add_connection_role';
-const ADD_CONNECTION_ID_KEY = 'meergo_ui_add_connection_id';
-const IS_PASSWORDLESS_KEY = 'meergo_ui_is_passwordless';
-const IS_DOCKER_KEY = 'meergo_ui_is_docker';
-const USERS_TAB_KEY = 'meergo_ui_users_tab';
-const USERS_EXPANDED_TRAITS_KEY = 'meergo_ui_users_expanded_traits';
-const USERS_PROPERTIES_KEY = 'meergo_ui_users_properties';
+const WORKSPACE_ID_KEY = 'meergo_admin_workspace_id';
+const ADD_CONNECTOR_CODE_KEY = 'meergo_admin_add_connector_code';
+const ADD_CONNECTION_ROLE_KEY = 'meergo_admin_add_connection_role';
+const ADD_CONNECTION_ID_KEY = 'meergo_admin_add_connection_id';
+const IS_PASSWORDLESS_KEY = 'meergo_admin_is_passwordless';
+const IS_DOCKER_KEY = 'meergo_admin_is_docker';
+const USERS_TAB_KEY = 'meergo_admin_users_tab';
+const USERS_EXPANDED_TRAITS_KEY = 'meergo_admin_users_expanded_traits';
+const USERS_PROPERTIES_KEY = 'meergo_admin_users_properties';
 
 // storageKeysToBeRemoved contains the list of keys in browser localStorage that
 // can be cleared when resetting the client state, for example to attempt fixing
