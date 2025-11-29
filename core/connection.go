4f83bc3892cec525912bc66bdf5ef0c80c9ca2e4
Marco Gazerro <gazerro@open2b.com>
2025-11-29 13:55:22 +0100

    core: make `PreviewSendEvent` preserve object key order
    Use the new 'Index' function from the 'core/json' package to indent JSON
    bodies while preserving the order of object keys.

--- core/connection.go
+++ core/connection.go
@@ -1623,7 +1623,7 @@ func (this *Connection) PreviewSendEvent(ctx context.Context, typ string, event 
 		}
 		switch ct {
 		case "application/json":
-			indented, err := json.IndentSorted(body, "", "  ")
+			indented, err := json.Indent(body, "", "  ")
 			if err != nil {
 				b.Write(body)
 				break
@@ -1647,7 +1647,7 @@ func (this *Connection) PreviewSendEvent(ctx context.Context, typ string, event 
 				if !first {
 					b.WriteByte('\n')
 				}
-				indented, err := json.IndentSorted(value, "", "    ")
+				indented, err := json.Indent(value, "", "    ")
 				if err != nil {
 					break
 				}
