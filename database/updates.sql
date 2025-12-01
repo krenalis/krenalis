0b6feba2e126418139df3e01b91a0cedf2e869bb
Marco Gazerro <gazerro@open2b.com>
2025-12-01 15:05:47 +0100

    Update `database/updates.sql` file

--- database/updates.sql
+++ database/updates.sql
@@ -99,3 +99,21 @@ WHERE in_schema IS NOT NULL AND  in_schema::text LIKE '%"maxByteLength": %';
 UPDATE pipelines
 SET out_schema = REPLACE(out_schema::text, '"maxByteLength": ', '"maxBytes": ')::jsonb
 WHERE out_schema IS NOT NULL AND  out_schema::text LIKE '%"maxByteLength": %';
+
+-- replace uint with unsigned int
+
+UPDATE workspaces
+SET alter_profile_schema_schema = REPLACE(alter_profile_schema_schema::text, '"kind": "uint"', '"kind": "int", "unsigned": true')::jsonb
+WHERE alter_profile_schema_schema IS NOT NULL AND alter_profile_schema_schema::text LIKE '%"kind": "uint"%';
+
+UPDATE workspaces
+SET profile_schema = REPLACE(profile_schema::text, '"kind": "uint"', '"kind": "int", "unsigned": true')::jsonb
+WHERE profile_schema::text LIKE '%"kind": "uint"%';
+
+UPDATE pipelines
+SET in_schema = REPLACE(in_schema::text, '"kind": "uint"', '"kind": "int", "unsigned": true')::jsonb
+WHERE in_schema IS NOT NULL AND  in_schema::text LIKE '%"kind": "uint"%';
+
+UPDATE pipelines
+SET out_schema = REPLACE(out_schema::text, '"kind": "uint"', '"kind": "int", "unsigned": true')::jsonb
+WHERE out_schema IS NOT NULL AND  out_schema::text LIKE '%"kind": "uint"%';
