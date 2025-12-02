70c4265223f9adf0aff467da65104fa8864da982
Gianluca Mondini <mondini@open2b.com>
2025-12-02 11:21:19 +0100

    meergo: move the Docker Compose file for release under 'tools'

--- test/commit/commit.go
+++ test/commit/commit.go
@@ -80,7 +80,7 @@ func main() {
 
 	// Validate the Docker Compose files.
 	NewCmd("docker", "compose", "config", "--quiet").InDir(repo).Run()
-	NewCmd("docker", "compose", "-f", "compose-release.yaml", "config", "--quiet").InDir(repo).Run()
+	NewCmd("docker", "compose", "-f", "tools/docker-compose/compose-release.yaml", "config", "--quiet").InDir(repo).Run()
 
 	// Run Go tests.
 	if runGoTests := !cliOptions.noGoTest; runGoTests {
