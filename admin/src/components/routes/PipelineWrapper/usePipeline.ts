krenalis/krenalis

commit 6bf0b70b612a87c87eb712cd8cf6e51d743b9d4d

admin: correctly handle pipelines without transformations
Close #2047

admin/src/lib/core/pipeline.ts
@@ -572,8 +572,10 @@ const transformPipelineMapping = (mapping: Mapping, outputSchema: ObjectType): T
 };
 
 const transformPipeline = (pipeline: Pipeline, outputSchema: ObjectType): TransformedPipeline => {
-	let pipelineMapping = pipeline.transformation.mapping;
-	if (pipeline.transformation.function == null && pipelineMapping == null) {
+	const hasTransformation = pipeline.transformation != null;
+
+	let pipelineMapping = null;
+	if (hasTransformation && pipeline.transformation.function == null && pipelineMapping == null) {
 		// Mappings are selected but there is nothing mapped.
 		pipelineMapping = {};
 	}
@@ -624,7 +626,7 @@ const transformPipeline = (pipeline: Pipeline, outputSchema: ObjectType): Transf
 		filter: pipeline.filter,
 		transformation: {
 			mapping: pipelineMapping != null ? transformPipelineMapping(pipelineMapping, outputSchema) : null,
-			function: pipeline.transformation.function,
+			function: hasTransformation ? pipeline.transformation.function : null,
 		},
 		query: pipeline.query,
 		path: pipeline.path,
@@ -1132,10 +1134,7 @@ const transformInPipelineToSet = async (
 		filter: filter,
 		inSchema: inSchema && inSchema.properties.length > 0 ? inSchema : null,
 		outSchema: outSchema && outSchema.properties.length > 0 ? outSchema : null,
-		transformation: {
-			mapping: mapping,
-			function: func,
-		},
+		transformation: mapping == null && func == null ? null : { mapping: mapping, function: func },
 		query: query!,
 		path: pipeline.path,
 		tableName: pipeline.tableName,

admin/src/lib/api/api.ts
@@ -251,15 +251,6 @@ class Connections {
 		if (!('linkedConnections' in c)) {
 			c.linkedConnections = null;
 		}
-		// Transform the 'pipelines.transformation' field to match the expected format used throughout the rest of the codebase.
-		for (let pipeline of c.pipelines) {
-			if (pipeline.transformation == null) {
-				pipeline.transformation = {
-					mapping: null,
-					function: null,
-				};
-			}
-		}
 		return c as Connection;
 	};
 
@@ -442,12 +433,6 @@ class Connections {
 		eventType: string,
 		pipeline: PipelineToSet,
 	): Promise<number> => {
-		// Transform the 'pipelines.transformation' field to match the API expected format.
-		if ('transformation' in pipeline) {
-			if (pipeline.transformation.mapping == null && pipeline.transformation.function == null) {
-				pipeline.transformation = null;
-			}
-		}
 		return await call(`${this.apiURL}/pipelines`, http.POST, this.workspaceID, {
 			connection,
 			target,
@@ -457,12 +442,6 @@ class Connections {
 	};
 
 	updatePipeline = async (id: number, pipeline: PipelineToSet): Promise<void> => {
-		// Transform the 'pipelines.transformation' field to match the API expected format.
-		if ('transformation' in pipeline) {
-			if (pipeline.transformation.mapping == null && pipeline.transformation.function == null) {
-				pipeline.transformation = null;
-			}
-		}
 		return await call(`${this.apiURL}/pipelines/${encodeURIComponent(id)}`, http.PUT, this.workspaceID, pipeline);
 	};
 

commit fb80d646842d23ab3bb6687ab634a4d2db197f26

admin: handle pipelines with optional transformation
This commit fixes an issue introduced by commit 6bf0b70, whereby pipelines with
optional transformation were in some cases handled in the same way as those that
did not support transformation at all, leading to unexpected behavior on the
pipeline page.

admin/src/components/routes/PipelineWrapper/usePipeline.ts
@@ -248,7 +248,11 @@ const usePipeline = (
 
 			let transformedPipeline: TransformedPipeline;
 			if (isEditing) {
-				transformedPipeline = transformPipeline(providedPipeline, outputSchema);
+				transformedPipeline = transformPipeline(
+					providedPipeline,
+					outputSchema,
+					fields.includes('Transformation'),
+				);
 				if (transformedPipeline.transformation.function != null) {
 					// Set the initial value of the selected properties
 					// of the function.

admin/src/lib/core/pipeline.ts
@@ -571,12 +571,19 @@ const transformPipelineMapping = (mapping: Mapping, outputSchema: ObjectType): T
 	return s;
 };
 
-const transformPipeline = (pipeline: Pipeline, outputSchema: ObjectType): TransformedPipeline => {
-	const hasTransformation = pipeline.transformation != null;
-
-	let pipelineMapping = null;
-	if (hasTransformation && pipeline.transformation.function == null && pipelineMapping == null) {
-		// Mappings are selected but there is nothing mapped.
+const transformPipeline = (
+	pipeline: Pipeline,
+	outputSchema: ObjectType,
+	supportsTransformation: boolean,
+): TransformedPipeline => {
+	let pipelineMapping = pipeline.transformation?.mapping;
+	if (
+		supportsTransformation &&
+		(pipeline.transformation == null ||
+			(pipeline.transformation.mapping == null && pipeline.transformation.function == null))
+	) {
+		// Mappings are selected but empty because the transformation is
+		// optional in this pipeline.
 		pipelineMapping = {};
 	}
 
@@ -626,7 +633,7 @@ const transformPipeline = (pipeline: Pipeline, outputSchema: ObjectType): Transf
 		filter: pipeline.filter,
 		transformation: {
 			mapping: pipelineMapping != null ? transformPipelineMapping(pipelineMapping, outputSchema) : null,
-			function: hasTransformation ? pipeline.transformation.function : null,
+			function: pipeline.transformation?.function,
 		},
 		query: pipeline.query,
 		path: pipeline.path,