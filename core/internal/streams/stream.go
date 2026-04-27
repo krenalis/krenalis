2dc54798fe6397514466206c653f6560068845cb
Marco Gazerro <gazerro@open2b.com>
2026-04-27 08:48:46 +0200

    Improve streams comments

--- core/internal/streams/stream.go
+++ core/internal/streams/stream.go
@@ -4,48 +4,49 @@
 
 package streams
 
-import "context"
+import (
+	"context"
+)
 
-// Stream is an interface for event streams.
-//
-// A Stream is safe for concurrent use by multiple goroutines, except that
-// Consume must not be called concurrently with another Consume call.
+// Stream represents an event stream.
+// Only Batch and WaitUp are safe for concurrent use by multiple goroutines.
 type Stream interface {
-
-	// Close closes the stream. When Close is called, no other calls to
-	// Stream's methods should be in progress and no other shall be made.
-	Close() error
-
-	// WaitUp blocks until the connection is up and the stream is available.
-	// It returns false if the context is canceled, the connection is closed,
-	// or the connection remains down for too long.
-	WaitUp(context.Context) bool
 
 	// Batch returns a batch publisher for the stream.
 	//
 	// It waits until the stream has been created. It returns an error only if ctx
-	//	is canceled or if the stream has been closed.
+	// is canceled or the stream has been closed.
+	//
+	// It is safe for concurrent use by multiple goroutines.
 	Batch(ctx context.Context) (BatchPublisher, error)
+
+	// Close closes the stream. When Close is called, no other calls to the
+	// Stream's methods should be in progress, and no further calls should be made.
+	Close() error
 
 	// Consume returns a buffered channel of the given size that streams events for
 	// the specified topic. Events belonging to the same shard are sent on the
 	// channel in order, ensuring per-user ordering is preserved.
 	Consume(topic string, size int) Consumer
+
+	// WaitUp blocks until the connection is up and the stream is available.
+	// It returns false if the context is canceled, the connection is closed,
+	// or the connection remains down for too long.
+	//
+	// It is safe for concurrent use by multiple goroutines.
+	WaitUp(context.Context) bool
 }
 
 // Consumer receives events for a pipeline.
-//
-// A Consumer must not be used concurrently by multiple goroutines. Different
-// Consumers may be used concurrently.
 type Consumer interface {
 
-	// Close closes the consumer and closes the events channel.
+	// Close closes the consumer and its events channel.
 	Close()
 
 	// Events returns the channel of events.
 	//
 	// It waits until the stream has been created. It returns an error only if ctx
-	//	is canceled or if the stream has been closed.
+	// is canceled or the stream has been closed.
 	Events(ctx context.Context) (<-chan Event, error)
 }
 
@@ -55,8 +56,8 @@ type Consumer interface {
 // Different BatchPublishers may be used concurrently.
 type BatchPublisher interface {
 
-	// Publish adds an event to the current batch for the given topic.
-	// If the topic begins with "connection-", destinations contains the destination
+	// Publish adds an event to the current batch for the given topics.
+	// If a topic begins with "connection-", destinations contains the destination
 	// pipelines the event is sent to.
 	Publish(ctx context.Context, topics []string, event map[string]any, destinations []int) error
 
