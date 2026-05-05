beb78d428d89cd7e0dd235b1bff9d614ced4dabc
Marco Gazerro <gazerro@open2b.com>
2026-05-05 14:50:12 +0200

    Fix license

--- warehouses/snowflake/internal/readonlysql/validate_test.go
+++ warehouses/snowflake/internal/readonlysql/validate_test.go
@@ -1,5 +1,5 @@
 // Copyright 2026 Open2b. All rights reserved.
-// Use of this source code is governed by an Elastic License 2.0
+// Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
 package readonlysql
