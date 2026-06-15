55f081a06392d952f4a78e0550e7ccb44a035d30
Marco Gazerro <gazerro@open2b.com>
2026-06-15 15:33:24 +0200

    Simplify transaction code

--- core/pipeline.go
+++ core/pipeline.go
@@ -890,17 +890,11 @@ func (this *Pipeline) createRun(ctx context.Context, incremental *bool) (string,
 		n.ID = generateID[any](nil)
 		err := this.core.state.Transaction(ctx, func(tx *db.Tx) (any, error) {
 			var function string
-			var enabled, inc, executing bool
+			var enabled, inc bool
 			var cursor time.Time
-			// Verify that a run can be created (the pipeline exists, is enabled,
-			// and has no run in progress) and load the settings needed to
-			// initialize it.
-			err := tx.QueryRow(ctx, "SELECT p.enabled, p.transformation_id, p.incremental, p.cursor, e.id IS NOT NULL AND e.end_time IS NULL\n"+
-				"FROM pipelines AS p\n"+
-				"LEFT JOIN pipelines_runs AS e ON p.id = e.pipeline\n"+
-				"WHERE p.id = $1\n"+
-				"ORDER BY e.id DESC\n"+
-				"LIMIT 1", n.Pipeline).Scan(&enabled, &function, &inc, &cursor, &executing)
+			// Verify that the pipeline is enabled and load the settings needed to initialize it.
+			err := tx.QueryRow(ctx, "SELECT enabled, transformation_id, incremental, cursor FROM pipelines WHERE id = $1",
+				n.Pipeline).Scan(&enabled, &function, &inc, &cursor)
 			if err != nil {
 				if err == sql.ErrNoRows {
 					return nil, errors.NotFound("pipeline %s does not exist", n.Pipeline)
@@ -909,9 +903,6 @@ func (this *Pipeline) createRun(ctx context.Context, incremental *bool) (string,
 			}
 			if !enabled {
 				return nil, errors.Unprocessable(PipelineDisabled, "pipeline %s is disabled", this.pipeline.ID)
-			}
-			if executing {
-				return nil, errors.Unprocessable(RunInProgress, "pipeline %s is already in progress", this.pipeline.ID)
 			}
 			if incremental == nil {
 				n.Incremental = inc
