b9ab09ea0f329f58bc7d784bd8f791b6060cdb2e
Marco Gazerro <gazerro@open2b.com>
2026-09-26 12:17:31 +0200

    core/internal/schemas: format schema alignment errors for UI rendering
    Wrap property names, types, and values in schema alignment errors with
    code-style delimiters.

--- core/internal/schemas/alignment.go
+++ core/internal/schemas/alignment.go
@@ -7,6 +7,7 @@ package schemas
 import (
 	"fmt"
 	"slices"
+	"strconv"
 
 	"github.com/krenalis/krenalis/core/internal/state"
 	"github.com/krenalis/krenalis/tools/types"
@@ -44,17 +45,17 @@ func CheckAlignment(t1, t2 types.Type, exportMode *state.ExportMode) error {
 		}
 		for path, p := range t2.Properties().WalkAll() {
 			if *exportMode != state.UpdateOnly && p.CreateRequired {
-				return &Error{Msg: fmt.Sprintf("%q property is required for creation", path)}
+				return &Error{Msg: fmt.Sprintf("«%s» property is required for creation", path)}
 			}
 			if *exportMode != state.CreateOnly && p.UpdateRequired {
-				return &Error{Msg: fmt.Sprintf("%q property is required for update", path)}
+				return &Error{Msg: fmt.Sprintf("«%s» property is required for update", path)}
 			}
 		}
 		return nil
 	}
 	if t2.Kind() == types.InvalidKind {
 		for _, p := range t1.Properties().All() {
-			return &Error{Msg: fmt.Sprintf("%q property no longer exists", p.Name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property no longer exists", p.Name)}
 		}
 	}
 	return checkTypeAlignment("", t1, t2, exportMode)
@@ -67,7 +68,7 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 	k2 := t2.Kind()
 	if k1 == types.ObjectKind {
 		if k2 != types.ObjectKind {
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from object to %s", name, t2)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «object» to «%s»", name, t2)}
 		}
 		properties1 := t1.Properties()
 		properties2 := t2.Properties()
@@ -78,33 +79,33 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 			}
 			p2, ok := properties2.ByName(p1.Name)
 			if !ok {
-				return &Error{Msg: fmt.Sprintf("%q property no longer exists", path)}
+				return &Error{Msg: fmt.Sprintf("«%s» property no longer exists", path)}
 			}
 			if exportMode != nil {
 				if p1.CreateRequired != p2.CreateRequired && *exportMode != state.UpdateOnly {
 					if p1.CreateRequired {
-						return &Error{Msg: fmt.Sprintf("%q property was previously required for creation but is no longer", path)}
+						return &Error{Msg: fmt.Sprintf("«%s» property was previously required for creation but is no longer", path)}
 					}
-					return &Error{Msg: fmt.Sprintf("%q property was not previously required for creation but it is now required", path)}
+					return &Error{Msg: fmt.Sprintf("«%s» property was not previously required for creation but it is now required", path)}
 				}
 				if p1.UpdateRequired != p2.UpdateRequired && *exportMode != state.CreateOnly {
 					if p1.UpdateRequired {
-						return &Error{Msg: fmt.Sprintf("%q property was previously required for the update but is no longer", path)}
+						return &Error{Msg: fmt.Sprintf("«%s» property was previously required for the update but is no longer", path)}
 					}
-					return &Error{Msg: fmt.Sprintf("%q property was not previously required for the update but it is now required", path)}
+					return &Error{Msg: fmt.Sprintf("«%s» property was not previously required for the update but it is now required", path)}
 				}
 			}
 			if exportMode == nil && p1.ReadOptional != p2.ReadOptional {
 				if p1.ReadOptional {
-					return &Error{Msg: fmt.Sprintf("%q property was previously optional but it is now non-optional", path)}
+					return &Error{Msg: fmt.Sprintf("«%s» property was previously optional but it is now non-optional", path)}
 				}
-				return &Error{Msg: fmt.Sprintf("%q property was previously non-optional but it is now optional", path)}
+				return &Error{Msg: fmt.Sprintf("«%s» property was previously non-optional but it is now optional", path)}
 			}
 			if p1.Nullable != p2.Nullable {
 				if p1.Nullable {
-					return &Error{Msg: fmt.Sprintf("%q property was previously nullable but it is no longer nullable", path)}
+					return &Error{Msg: fmt.Sprintf("«%s» property was previously nullable but it is no longer nullable", path)}
 				}
-				return &Error{Msg: fmt.Sprintf("%q property was previously non-nullable but is now nullable", path)}
+				return &Error{Msg: fmt.Sprintf("«%s» property was previously non-nullable but is now nullable", path)}
 			}
 			err := checkTypeAlignment(path, p1.Type, p2.Type, exportMode)
 			if err != nil {
@@ -124,9 +125,9 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 				continue
 			}
 			if checkCreate {
-				return &Error{Msg: fmt.Sprintf(`"%s.%s" property is required for creation but is not present in the schema`, name, p.Name)}
+				return &Error{Msg: fmt.Sprintf(`«%s.%s» property is required for creation but is not present in the schema`, name, p.Name)}
 			}
-			return &Error{Msg: fmt.Sprintf(`"%s.%s" property is required for update but is not present in the schema`, name, p.Name)}
+			return &Error{Msg: fmt.Sprintf(`«%s.%s» property is required for update but is not present in the schema`, name, p.Name)}
 		}
 		return nil
 	}
@@ -134,51 +135,51 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 		return nil
 	}
 	if k1 != k2 {
-		return &Error{Msg: fmt.Sprintf("%q property's type has changed from %s to %s", name, t1, t2)}
+		return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «%s» to «%s»", name, t1, t2)}
 	}
 	switch k1 {
 	case types.StringKind:
 		c1, ok1 := t1.MaxLength()
 		c2, ok2 := t2.MaxLength()
 		if c1 != c2 || ok1 != ok2 {
-			var v1, v2 any = "unbounded", "unbounded"
+			var v1, v2 = "unbounded", "unbounded"
 			if ok1 {
-				v1 = c1
+				v1 = "«" + strconv.Itoa(c1) + "»"
 			}
 			if ok2 {
-				v2 = c2
+				v2 = "«" + strconv.Itoa(c2) + "»"
 			}
-			return &Error{Msg: fmt.Sprintf("character length of the %q property's type has changed from %v to %v", name, v1, v2)}
+			return &Error{Msg: fmt.Sprintf("character length of the «%s» property's type has changed from %s to %s", name, v1, v2)}
 		}
 		b1, ok1 := t1.MaxBytes()
 		b2, ok2 := t2.MaxBytes()
 		if b1 != b2 || ok1 != ok2 {
-			var v1, v2 any = "unbounded", "unbounded"
+			var v1, v2 = "unbounded", "unbounded"
 			if ok1 {
-				v1 = b1
+				v1 = "«" + strconv.Itoa(b1) + "»"
 			}
 			if ok2 {
-				v2 = b2
+				v2 = "«" + strconv.Itoa(b2) + "»"
 			}
-			return &Error{Msg: fmt.Sprintf("byte length of the %q property's type has changed from %v to %v", name, v1, v2)}
+			return &Error{Msg: fmt.Sprintf("byte length of the «%s» property's type has changed from %s to %s", name, v1, v2)}
 		}
 		vs1 := t1.Values()
 		vs2 := t2.Values()
 		if vs1 != nil && vs2 == nil {
-			return &Error{Msg: fmt.Sprintf("%q property was previously limited to specific values but is now unrestricted", name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property was previously limited to specific values but is now unrestricted", name)}
 		}
 		if vs1 == nil && vs2 != nil {
-			return &Error{Msg: fmt.Sprintf("%q property was previously unrestricted but is now limited to specific values", name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property was previously unrestricted but is now limited to specific values", name)}
 		}
 		for _, v1 := range vs1 {
 			if !slices.Contains(vs2, v1) {
-				return &Error{Msg: fmt.Sprintf("%q property allowed value %q but it is no longer allowed", name, v1)}
+				return &Error{Msg: fmt.Sprintf("«%s» property allowed value «%s» but it is no longer allowed", name, v1)}
 			}
 		}
 		if len(vs1) < len(vs2) {
 			for _, v2 := range vs2 {
 				if !slices.Contains(vs1, v2) {
-					return &Error{Msg: fmt.Sprintf("%q property previously disallowed value %q but it now allows it", name, v2)}
+					return &Error{Msg: fmt.Sprintf("«%s» property previously disallowed value «%s» but it now allows it", name, v2)}
 				}
 			}
 		}
@@ -191,52 +192,52 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 		if re2 != nil {
 			v2 = `"` + re2.String() + `"`
 		}
-		return &Error{Msg: fmt.Sprintf("regular expression of the %q property's type has changed from %s to %s", name, v1, v2)}
+		return &Error{Msg: fmt.Sprintf("regular expression of the «%s» property's type has changed from %s to %s", name, v1, v2)}
 	case types.IntKind:
 		if t1.IsUnsigned() != t2.IsUnsigned() {
 			if t1.IsUnsigned() {
-				return &Error{Msg: fmt.Sprintf("%q property's type has changed from unsigned %s to %s", name, t1, t2)}
+				return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «unsigned %s» to «%s»", name, t1, t2)}
 			}
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from %s to unsigned %s", name, t1, t2)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «%s» to «unsigned %s»", name, t1, t2)}
 		}
 		if t1.BitSize() != t2.BitSize() {
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from %s to %s", name, t1, t2)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «%s» to «%s»", name, t1, t2)}
 		}
 		if t1.IsUnsigned() {
 			min1, max1 := t1.UnsignedRange()
 			min2, max2 := t2.UnsignedRange()
-			return &Error{Msg: fmt.Sprintf("range of the %q property's type has changed from [%d,%d] to [%d,%d]", name, min1, max1, min2, max2)}
+			return &Error{Msg: fmt.Sprintf("range of the «%s» property's type has changed from [%d,%d] to [%d,%d]", name, min1, max1, min2, max2)}
 		}
 		min1, max1 := t1.IntRange()
 		min2, max2 := t2.IntRange()
-		return &Error{Msg: fmt.Sprintf("range of the %q property's type has changed from [%d,%d] to [%d,%d]", name, min1, max1, min2, max2)}
+		return &Error{Msg: fmt.Sprintf("range of the «%s» property's type has changed from [%d,%d] to [%d,%d]", name, min1, max1, min2, max2)}
 	case types.FloatKind:
 		if t1.BitSize() != t2.BitSize() {
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from %s to %s", name, t1, t2)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from «%s» to «%s»", name, t1, t2)}
 		}
 		if t1.IsReal() && !t2.IsReal() {
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from real to non-real", name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from real to non-real", name)}
 		}
 		if !t1.IsReal() && t2.IsReal() {
-			return &Error{Msg: fmt.Sprintf("%q property's type has changed from non-real to real", name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property's type has changed from non-real to real", name)}
 		}
 		min1, max1 := t1.FloatRange()
 		min2, max2 := t2.FloatRange()
-		return &Error{Msg: fmt.Sprintf("range of the %q property's type has changed from [%g,%g] to [%g,%g]", name, min1, max1, min2, max2)}
+		return &Error{Msg: fmt.Sprintf("range of the «%s» property's type has changed from [%g,%g] to [%g,%g]", name, min1, max1, min2, max2)}
 	case types.DecimalKind:
 		p1 := t1.Precision()
 		p2 := t2.Precision()
 		if p1 != p2 {
-			return &Error{Msg: fmt.Sprintf("precision of the %q property's type has changed from %d to %d", name, p1, p2)}
+			return &Error{Msg: fmt.Sprintf("precision of the «%s» property's type has changed from %d to %d", name, p1, p2)}
 		}
 		s1 := t1.Scale()
 		s2 := t2.Scale()
 		if s1 != s2 {
-			return &Error{Msg: fmt.Sprintf("scale of the %q property's type has changed from %d to %d", name, s1, s2)}
+			return &Error{Msg: fmt.Sprintf("scale of the «%s» property's type has changed from %d to %d", name, s1, s2)}
 		}
 		min1, max1 := t1.DecimalRange()
 		min2, max2 := t2.DecimalRange()
-		return &Error{Msg: fmt.Sprintf("range of %q property's type has changed from [%s,%s] to [%s,%s]", name, min1, max1, min2, max2)}
+		return &Error{Msg: fmt.Sprintf("range of «%s» property's type has changed from [%s,%s] to [%s,%s]", name, min1, max1, min2, max2)}
 	case types.ArrayKind:
 		err := checkTypeAlignment(name+"[]", t1.Elem(), t2.Elem(), exportMode)
 		if err != nil {
@@ -245,17 +246,17 @@ func checkTypeAlignment(name string, t1, t2 types.Type, exportMode *state.Export
 		n1 := t1.MinElements()
 		n2 := t2.MinElements()
 		if n1 != n2 {
-			return &Error{Msg: fmt.Sprintf("minimum number of %q property elements has been changed from %d to %d", name, n1, n2)}
+			return &Error{Msg: fmt.Sprintf("minimum number of «%s» property elements has been changed from %d to %d", name, n1, n2)}
 		}
 		n1 = t1.MaxElements()
 		n2 = t2.MaxElements()
 		if n1 != n2 {
-			return &Error{Msg: fmt.Sprintf("minimum number of %q property elements has been changed from %d to %d", name, n1, n2)}
+			return &Error{Msg: fmt.Sprintf("minimum number of «%s» property elements has been changed from %d to %d", name, n1, n2)}
 		}
 		if t1.Unique() {
-			return &Error{Msg: fmt.Sprintf("%q property elements were initially required to be unique, but it is no longer required", name)}
+			return &Error{Msg: fmt.Sprintf("«%s» property elements were initially required to be unique, but it is no longer required", name)}
 		}
-		return &Error{Msg: fmt.Sprintf("%q property elements were not required to be unique, but now it is required", name)}
+		return &Error{Msg: fmt.Sprintf("«%s» property elements were not required to be unique, but now it is required", name)}
 	case types.MapKind:
 		return checkTypeAlignment(name+"[]", t1.Elem(), t2.Elem(), exportMode)
 	default:
